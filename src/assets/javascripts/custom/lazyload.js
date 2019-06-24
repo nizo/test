82d91609032aed6e2d714f241128f20bafea9292
Tobias Lucas <tobias.lucas@callone.de>
2019-06-24 17:35:53 +0200

    add lazyLoad for picture element
    add playholder image
    add webp images

--- src/assets/javascripts/custom/lazyload.js
+++ src/assets/javascripts/custom/lazyload.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
   var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazyBackground"));
+  var lazyPictures = [].slice.call(document.querySelectorAll(".lazyPicture > source"));
   var active = false;
 
   const lazyLoad = function() {
@@ -33,6 +34,26 @@ document.addEventListener("DOMContentLoaded", function() {
         	if ((lazyBackground.getBoundingClientRect().top <= window.innerHeight + 500 && lazyBackground.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyBackground).display !== "none") {
             	lazyBackground.classList.add("visible");
             }      
+        });
+        
+        lazyPictures.forEach(function(lazyPicture) {
+        	lazyPicture.style.visibility = "hidden";
+        	
+        	if ((lazyPicture.getBoundingClientRect().top <= window.innerHeight && lazyPicture.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyPicture).display !== "none") {
+        		lazyPicture.srcset = lazyPicture.dataset.srcset;
+        		lazyPicture.parentElement.classList.remove("lazy");
+        		lazyPicture.style.visibility = "visible";
+        		
+        		lazyPicture = lazyPicture.filter(function(image) {
+                    return image !== lazyPicture;
+                  });
+
+                  if (lazyPicture.length === 0) {
+                    document.removeEventListener("scroll", lazyLoad);
+                    window.removeEventListener("resize", lazyLoad);
+                    window.removeEventListener("orientationchange", lazyLoad);
+                  }
+            }     
         });
         
         active = false;
