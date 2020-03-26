c11a6636ac9991452a96a71f3083fada60d53071
Tobias Lucas <tobias.lucas@callone.de>
2020-03-26 13:53:42 +0100

    add new background gradient for test site

--- src/views/de/testseite.php
+++ src/views/de/testseite.php
@@ -107,7 +107,7 @@
 
 <div class="wrapper background-image image4 lazyBackground centered no-margin relative">
 	<div class="content narrow centered no-margin-bottom no-margin-top">
-		<img src="/assets/images/photos/placeholder.gif" data-src="/assets/animations/dashboard.gif" data-srcset="/assets/animations/dashboard.gif" class="lazy agentboardImage" alt="Unsere Apps für Ihr Unternehmen" title="Das CallOne App-Universum" />          	      
+		<img src="/assets/images/photos/placeholder.gif" data-src="/assets/animations/callcenter-software-animation.gif" data-srcset="/assets/animations/callcenter-software-animation.gif" class="lazy agentboardImage" alt="Unsere Apps für Ihr Unternehmen" title="Das CallOne App-Universum" />          	      
 	</div>
 </div>	
 
