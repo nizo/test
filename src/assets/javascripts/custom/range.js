f28a62ac4189afecc581f13889a87954c257b7dd
Max Boll <max.boll@callone.de>
2021-06-08 17:47:11 +0200

    Added more HTML/CSS/JS to new contact-sales page Added Script for custom range inputs

---
+++ src/assets/javascripts/custom/range.js
@@ -0,0 +1,126 @@
+/*
+* @title         Custom Range Slider
+* @author        Maximilian Boll
+* @version       1.0.0
+* @description   A lightweight javascript class that transforms input type range
+*                sliders to custom sliders that can be modified in styling
+* @licence       Copyright by CallOne GmbH, all rights reserved
+* @usage         <input type="range" min="0" max="100" step="1" value="25" calloneRange data-output="#outputId" />
+* @changelog     08. June 2021: Initial creation
+*/
+
+class Range {
+    constructor(slider) {
+        this.ns = 'callone-range';
+        this.slider = slider;
+        this.step = 100 / (this.slider.max / this.slider.step);
+        this.dragging = false;
+        this.wrapper = document.createElement('div');
+        this.track = document.createElement('div');
+        this.thumb = document.createElement('div');
+        this.active = document.createElement('div');
+        this.output = null;
+        if (this.slider.hasAttribute('data-output')) {
+            this.output = document.querySelector(this.slider.dataset.output);
+        }
+        if (this.output) {
+            this.setOutputValue();
+            this.slider.addEventListener('change', this.setOutputValue.bind(this));
+            this.output.addEventListener('keyup', this.setProgress.bind(this));
+        }
+        if (this.slider.hasAttribute('data-width')) {
+            this.wrapper.style.width = this.slider.dataset.width + 'px';
+        }
+        if (this.slider.hasAttribute('data-height')) {
+            this.wrapper.style.height = this.slider.dataset.height + 'px';
+        }
+        this.wrap();
+        this.addTrack();
+        this.addThumb();
+        this.addActive();
+        this.wrapper.addEventListener('click', this.changeProgress.bind(this));
+        this.thumb.addEventListener('mousedown', (() => {
+            this.dragging = true;
+        }).bind(this));
+        window.addEventListener('mouseup', (() => {
+            this.dragging = false;
+        }).bind(this));
+        window.addEventListener('mousemove', ((e) => {
+            if (this.dragging) {
+                this.changeProgress(e);
+            }
+        }).bind(this));
+    }
+
+    setOutputValue() {
+        this.output.value = this.slider.value;
+    }
+
+    setProgress() {
+        let newValue = parseInt(this.output.value);
+        if (!newValue) {
+            newValue = 0;
+        }
+        if (newValue < this.slider.min)
+            newValue = this.slider.min;
+        if (newValue > this.slider.max)
+            newValue = this.slider.max
+        this.output.value = newValue;
+        let progress = 100 / this.slider.max * newValue;
+        this.thumb.style.left = progress + '%';
+        this.active.style.width = progress + '%';
+        this.slider.value = this.slider.max * (progress / 100);
+    }
+    
+    changeProgress(e) {
+        let range = this.wrapper.getBoundingClientRect();
+        let mouse = {
+            x: e.pageX,
+            y: e.pageY
+        };
+        let progress = 100 / range.width * (mouse.x - range.x);
+        progress = progress - (progress % this.step);
+        if (progress < 0)
+            progress = 0
+        if (progress > 100)
+            progress = 100
+        this.thumb.style.left = progress + '%';
+        this.active.style.width = progress + '%';
+        this.slider.value = this.slider.max * (progress / 100);
+        let event = new Event('change');
+        this.slider.dispatchEvent(event);
+    }
+    
+    wrap() {
+        this.wrapper.classList.add(this.ns);
+        this.slider.parentNode.insertBefore(this.wrapper, this.slider);
+        this.wrapper.appendChild(this.slider);
+    }
+    
+    addTrack() {
+        this.track.classList.add(this.ns + '__track');
+        this.wrapper.appendChild(this.track);
+    }
+    
+    addThumb() {
+        this.thumb.classList.add(this.ns + '__thumb');
+        this.thumb.style.left = this.getProgressInPercent() + '%';
+        this.wrapper.appendChild(this.thumb);
+    }
+    
+    addActive() {
+        this.active.classList.add(this.ns + '__active');
+        this.active.style.width = this.getProgressInPercent() + '%';
+        this.wrapper.appendChild(this.active);
+    }
+    
+    getProgressInPercent() {
+        return 100 / this.slider.max * this.slider.value;
+    }
+}
+
+// Execute
+let sliders = document.querySelectorAll('[calloneRange]');
+sliders.forEach(slider => {
+    new Range(slider);
+});
--- src/views/de/contact_sales.php
+++ src/views/de/contact_sales.php
@@ -5,18 +5,50 @@
         <div class="contactsales__steps">
             <div class="contactsales__steps-header">
                 <div class="contactsales__steps-person">
-                    Björn Bendig
+                    <img src="/assets/images/photos/bjoern.svg" alt="" />
+                    <div>
+                        <strong>Björn Bendig</strong><br />
+                        Geschäftsführer von CallOne
+                    </div>
                 </div>
                 <div class="contactsales__steps-progress">
-                    Schritt 1/2
+                    <div>
+                        <span prevStep>Schritt zurück</span>
+                        <span>Schritt 1/2</span>
+                    </div>
                 </div>
             </div>
 
-            <div class="contactsales__step">
+            <div class="contactsales__step" data-step="1">
                 <h2>Wie groß ist Ihre Organisation?</h2>
-                <p>[SLIDER]</p>
-                <p>[NUMBER] Mitarbeiter</p>
-                <button>Weiter</button>
+                <input type="range" min="1" max="150" step="1" value="25" calloneRange data-output="#workers" data-width="480" />
+                <div>
+                    <input type="text" id="workers" /> Mitarbeiter
+                </div>
+                <button class="button button--wider tertiary black2 rounded" nextStep>Weiter</button>
+            </div>
+
+            <div class="contactsales__step contactsales__step--hidden" data-step="2">
+                <h2>Wie möchten Sie mit uns in Kontakt treten?</h2>
+                <div class="contactsales__options">
+                    <div class="contactsales__option" data-option="1">
+                        <img src="/assets/images/icons_svg/contact-sales-black.svg" alt="" />
+                        <h3>Vertrieb kontaktieren</h3>
+                        <p>Über Kontaktformular</p>
+                    </div>
+                    <div class="contactsales__option contactsales__option--active" data-option="2">
+                        <img src="/assets/images/icons_svg/callback-black.svg" alt="" />
+                        <h3>15 min Telefonbesprechung</h3>
+                        <p>Über Calendly Terminbuchung</p>
+                    </div>
+                </div>
+
+                <div class="contactsales__option-content contactsales__option-content--hidden" data-option="1">
+                    [CONTACTFORM]
+                </div>
+                <div class="contactsales__option-content" data-option="2">
+                    <button class="button tertiary black rounded">Jetzt Termin buchen</button>
+                </div>
             </div>
         </div>
     </div>
@@ -33,3 +65,45 @@
         <h1>Kundenmeinungen</h1>
     </div>
 </div>
+
+<script>
+let buttonNextStep = document.querySelector('[nextStep]');
+let buttonPrevStep = document.querySelector('[prevStep]');
+let contactPerson = document.querySelector('.contactsales__steps-person');
+let stepsProgress = document.querySelector('.contactsales__steps-progress');
+let stepsProgressText = stepsProgress.querySelector('span:last-of-type');
+let step1 = document.querySelector('.contactsales__step[data-step="1"]');
+let step2 = document.querySelector('.contactsales__step[data-step="2"]');
+let optionButtons = document.querySelectorAll('.contactsales__option');
+let options = document.querySelectorAll('.contactsales__option-content');
+buttonNextStep.addEventListener('click', e => {
+    e.preventDefault();
+    stepsProgress.classList.add('contactsales__steps-progress--full');
+    stepsProgressText.textContent = 'Schritt 2/2';
+    contactPerson.classList.add('contactsales__steps-person--hidden');
+    step1.classList.add('contactsales__step--hidden');
+    step2.classList.remove('contactsales__step--hidden');
+});
+buttonPrevStep.addEventListener('click', e => {
+    e.preventDefault();
+    stepsProgress.classList.remove('contactsales__steps-progress--full');
+    stepsProgressText.textContent = 'Schritt 1/2';
+    contactPerson.classList.remove('contactsales__steps-person--hidden');
+    step1.classList.remove('contactsales__step--hidden');
+    step2.classList.add('contactsales__step--hidden');
+});
+optionButtons.forEach(button => {
+    button.addEventListener('click', e => {
+        optionButtons.forEach(button => {
+            button.classList.remove('contactsales__option--active');
+        });
+        options.forEach(option => {
+            option.classList.add('contactsales__option-content--hidden');
+            if (button.dataset.option == option.dataset.option) {
+                option.classList.remove('contactsales__option-content--hidden');
+            }
+        });
+        button.classList.add('contactsales__option--active');
+    });
+});
+</script>
