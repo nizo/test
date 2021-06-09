5b4270ced220e70662a82e319a44353dbf89c5ee
Max Boll <max.boll@callone.de>
2021-06-09 17:56:52 +0200

    #fahK9ASF Added contact-sales form and created floating form component

--- src/views/de/contact_sales.php
+++ src/views/de/contact_sales.php
@@ -44,7 +44,67 @@
                 </div>
 
                 <div class="contactsales__option-content contactsales__option-content--hidden" data-option="1">
-                    [CONTACTFORM]
+                    <p>Unser Team beantwortet gerne alle Ihre vertrieblichen Fragen. Füllen Sie das Formular aus und wir setzen uns so schnell wie möglich mit Ihnen in Verbindung.</p>
+
+                    <form action="#" method="POST" class="floating-form">
+                        <div class="floating-form__row">
+                            <div class="floating-form__col">
+                                <div class="floating-form__field">
+                                    <input type="text" name="name" placeholder=" " />
+                                    <label>Ihr Name *</label>
+                                </div>
+                            </div>
+                            <div class="floating-form__col">
+                                <div class="floating-form__field">
+                                    <input type="text" name="company" placeholder=" " />
+                                    <label>Firmenname</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="floating-form__row">
+                            <div class="floating-form__col">
+                                <div class="floating-form__select">
+                                    <select name="contactperson" calloneSelect required>
+                                        <option value="">Sie sind Ansprechpartner für *</option>
+                                        <option value="Sales">Sales</option>
+                                        <option value="Finanzen">Finanzen</option>
+                                        <option value="Support">Support</option>
+                                        <option value="Technik">Technik</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="floating-form__col">
+                                <div class="floating-form__select">
+                                    <select name="challenge" calloneSelect required>
+                                        <option value="">Herausforderung Ihrer Firma *</option>
+                                        <option value="Wachstum &amp; Skalierung">Wachstum &amp; Skalierung</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="floating-form__row">
+                            <div class="floating-form__col">
+                                <div class="floating-form__field">
+                                    <input type="text" name="email" placeholder=" " />
+                                    <label>Geschäftliche E-Mail *</label>
+                                </div>
+                            </div>
+                            <div class="floating-form__col">
+                                <div class="floating-form__field">
+                                    <input type="text" name="phone" placeholder=" " />
+                                    <label>Geschäftliche Telefonnummer *</label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" class="floating-form__submit">An Vertrieb senden</button>
+                    </form>
+
+                    <div class="contactsales__option-footer">
+                        <p>Ihre Anforderungen sind so individuell, dass sie nicht zum Standardformular passen? CallOne liebt die kniffligen Telekommunikationsprobleme. So erreichen Sie uns anders:</p>
+
+                        <p>TODO: Telefon / Email</p>
+                    </div>
                 </div>
                 <div class="contactsales__option-content" data-option="2">
                     <button class="button tertiary black rounded">Jetzt Termin buchen</button>
@@ -52,7 +112,12 @@
             </div>
 
             <div class="contactsales__step contactsales__step--hidden" data-step="3">
-                Nicht genug Mitarbeiter
+                <h2>Wir sind noch nicht so weit &hellip;</h2>
+                <p>Danke für die Anfrage, wir fühlen uns geschmeichelt. Wir lieben kleine Unternehmen, sie sind das Rückgrat unserer Branche. Doch leider haben wir noch keine adäquaten Lösungen für sie parat.</p>
+                <p>Unsere Lösungen sind aktuell zu umfassend und zu teuer für Unternehmen unter 10 Mitarbeitenden. CallOne richtet sich an den Bedarf und die Herausforderungen mittelständischer Unternehmen.</p>
+                <p>Einige Empfehlungen sehr guter Anbieter für kleine Unternehmen finden Sie hier:</p>
+                <p>[BLOGPOST]</p>                    
+                <p>Alles Gute für die Zukunft und viel Glück mit Ihrem neuen Telefonie-Partner.</p>
             </div>
         </div>
     </div>
