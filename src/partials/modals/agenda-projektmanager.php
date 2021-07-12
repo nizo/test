aa1926a075f8468a176585f19174831510e56eb4
Max Boll <max.boll@callone.de>
2021-07-12 17:10:50 +0200

    Added more HTML structure and content to karriere bei callone page Added agenda modal #9zefWMGc

---
+++ src/partials/modals/agenda-projektmanager.php
@@ -0,0 +1,57 @@
+<div class="callone-modal callone-modal--no-padding" data-modal="agenda-projektmanager" data-title="Tagesagenda" data-subtitle="Projektmanager*in">
+    <div class="agenda">
+        <div class="agenda__title">Vormittag</div>
+
+        <div class="agenda__row">
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--cup">
+                    Tasse Kaffee oder Tee
+                </div>
+                <div class="agenda__item agenda__item--blue agenda__item--exchange">
+                    IT-Änderungen an Kundenprojekt mit Entwicklung organisieren
+                </div>
+            </div>
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--blue agenda__item--exchange">
+                    Daily Standup zur operativen Abstimmung
+                </div>
+            </div>
+        </div>
+
+        <div class="agenda__break">
+            <strong>Mittagspause</strong><br />
+            Gemeinsam als Team oder alleine
+        </div>
+
+        <div class="agenda__title">Nachmittag</div>
+
+        <div class="agenda__row">
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--green agenda__item--customer-support">
+                    Kunden-Support
+                </div>
+                <div class="agenda__item agenda__item--blue agenda__item--exchange">
+                    Neukunden und Administratives mit Geschäftsführer besprechen
+                </div>
+                <div class="agenda__item agenda__item--red agenda__item--computer">
+                    Unternehmensrepräsentation auf Website mitgestalten
+                </div>
+            </div>
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--red agenda__item--computer">
+                    Anforderungen der Kunden planen und umsetzen
+                </div>
+                <div class="agenda__item agenda__item--yellow agenda__item--exchange">
+                    Kommunikation mit CallOne Lieferanten
+                </div>
+            </div>
+        </div>
+
+        <div class="agenda__legend">
+            <div class="agenda__info agenda__info--blue">Interne Kommunikation</div>
+            <div class="agenda__info agenda__info--green">Kundenkommunikation</div>
+            <div class="agenda__info agenda__info--red">Computer-Arbeit</div>
+            <div class="agenda__info agenda__info--yellow">Lieferantenkommunikation</div>
+        </div>
+    </div>
+</div>
--- src/views/de/karriere-bei-callone.php
+++ src/views/de/karriere-bei-callone.php
@@ -45,9 +45,37 @@ $jobs = jobs_load();
             <a href="#" class="btn btn--secondary btn--centered">Offene Stellen <?php if (!empty($jobs)) {?><span class="btn__notification"><?= count($jobs); ?></span><?php } ?></a>
         </p>
     </div>
+
+    <div class="section__content section__content--wide">
+        <div class="grid centered">
+            <div class="grid__col--6-lg grid__col--2-md">
+                <img src="/assets/images/illus/costumer-support-negative-illustration.svg" alt="" />
+                <h2>Wir alle wissen es: Kundenhotlines sind die Hölle</h2>
+                <p>Kein Mensch, der den Support anrufen muss, liebt es. Lange Warteschleifen, Inkompetenz, Unfreundlichkeit, Zeitverschwendung, nichts als Ärger, Problem nicht gelöst. Das geht besser.</p>
+            </div>
+            <div class="grid__col--6 lg grid__col--8-md">
+                <img src="/assets/images/illus/costumer-support-positive-illustration.svg" alt="" />
+                <h2>Mit CallOne macht Kundenservice das erste Mal Spaß</h2>
+                <p>Mit Technologien aus der Zukunft geben wir Kunden wesentlich bessere, zielführende Supporterlebnisse. Jeder Anruf wird kürzer, angenehmer und lösungsorientierter. Und man legt mit einem Lächeln auf.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section" id="vorteile">
+    <div class="section__content section__content--narrow">
+        <h1 class="centered">Deinen Traumjob gestaltest du selbst</h1>
+
+        <p class="centered">Hier kommt dein Job mit Sinn, Substanz und Wochenende. Als mittelständisches Potsdamer IT-Unternehmen mit Anti-Bullshit-Philosophie sind wir etabliert, zukunftsorientiert, unabhängig und krisensicher. Nicht Hype, sondern Happiness zählt. Du entscheidest, was du dafür brauchst.</p>
+
+        <p>
+            <a href="#" class="btn btn--primary btn--centered">Offene Stellen <?php if (!empty($jobs)) {?><span class="btn__notification"><?= count($jobs); ?></span><?php } ?></a>
+        </p>
+    </div>
+
     <div class="section__content section__content--wide">
         <div class="cards">
-        <div class="cards__card cards__card--3">
+            <div class="cards__card cards__card--3">
                 <img src="/assets/images/icons_svg/benefits.svg" alt="" />
                 <h3>Bonbons gefällig?</h3>
                 <p>30 Tage Urlaub, fester Feierabend, ein Budget für deine Weiterentwicklung, Team-Events, E-Bike, Altersvorsorge und Gutscheine für Amazon & Co. sind einige der Schmankerl, die wir dir für deine gute Arbeit anbieten.</p>
@@ -81,23 +109,56 @@ $jobs = jobs_load();
     </div>
 </div>
 
-<div class="section" id="vorteile">
-    <div class="section__content section__content--narrow">
-        <h1 class="centered">Deinen Traumjob gestaltest du selbst</h1>
-
-        <p class="centered">Hier kommt dein Job mit Sinn, Substanz und Wochenende. Als mittelständisches Potsdamer IT-Unternehmen mit Anti-Bullshit-Philosophie sind wir etabliert, zukunftsorientiert, unabhängig und krisensicher. Nicht Hype, sondern Happiness zählt. Du entscheidest, was du dafür brauchst.</p>
-
-        <p>
-            <a href="#" class="btn btn--primary btn--centered">Offene Stellen <?php if (!empty($jobs)) {?><span class="btn__notification"><?= count($jobs); ?></span><?php } ?></a>
-        </p>
-    </div>
-</div>
-
 <div class="section section--dark-green-black" id="menschen">
     <div class="section__content section__content--narrow">
         <h1 class="centered">Ein Team, das hinter dir steht</h1>
 
         <p class="centered">Mit Humor, Persönlichkeit, verschiedensten Hintergründen, Fähigkeiten und Interessen arbeiten wir stark zusammen.</p>
+    </div>
+
+    <div class="section__content section__content--wide section__content--gutter-top">
+        <div class="team">
+            <div class="team__header">
+                <p>Menschen bei CallOne</p>
+                <div class="team__buttons">
+                    <div class="team__button team__button--active">
+                        <img src="https://picsum.photos/seed/avatar/200" alt="" />
+                    </div>
+                    <div class="team__button">
+                        <img src="https://picsum.photos/seed/team2/200" alt="" />
+                    </div>
+                    <div class="team__button">
+                        <img src="https://picsum.photos/seed/team3/200" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="team__content">
+                <div class="team__member team__member--active">
+                    <div class="team__member-left">
+                        <blockquote>„CallOne bietet mir viel Freiraum in meinem Job: Ich kann mich jederzeit einbringen und meine Ideen finden Gehör. Weiterbildung ist ausdrücklich erwünscht: Zurzeit lerne ich Programmieren und ein Kollege hilft mir dabei.”</blockquote>
+                        <h6>
+                            <strong>Charlotta Barth</strong><br />
+                            Projektmanagerin
+                        </h6>
+
+                        <div class="team__member-booking">
+                            <h3>Lust mehr über CallOne zu erfahren?</h3>
+                            <p>Buche jetzt ganz unkompliziert einen Kennenlerntermin mit <u>Charlotta</u>.</p>
+                            <a href="#" class="btn btn--border-white">Termin buchen</a>
+                        </div>
+                    </div>
+                    <div class="team__member-right">
+                        <div class="team__member-image">
+                            <img src="https://picsum.photos/seed/avatar/368/510" alt="" />
+                        </div>
+                        <div class="team__member-agenda" data-openmodal="agenda-projektmanager">
+                            <strong>Tagesagenda</strong><br />
+                            Projektmanagerin
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
