88255c218d2bf8866baa6a3415721d884efb696f
brian <brian.miculcy@callone.de>
2021-08-18 16:20:40 +0200

    update dataprotection page

--- src/views/de/datenschutz.php
+++ src/views/de/datenschutz.php
@@ -1,7 +1,7 @@
 <div class="wrapper">
   <div class="content narrow">
     <h1>Datenschutzhinweise<br/>der CallOne GmbH</h1>
-    <h2>Stand 22.05.2018</h2>
+    <h2>Stand 18.08.2021</h2>
 
     <span></span>
     <p>
@@ -227,7 +227,7 @@
     </p>
     <p><b>C. Ausübung des Widerspruchsrechts</b></p>
     <p>
-      Das Widerspruchsrecht kann formlos ausgeübt werden, etwa per Post an CallOne GmbH, Friedrich-König-Str. 25a, 55129 Mainz oder per E-Mail an <script id="mail_to-q11z0dcu">eval(decodeURIComponent('%76%61%72%20%73%63%72%69%70%74%20%3d%20%64%6f%63%75%6d%65%6e%74%2e%67%65%74%45%6c%65%6d%65%6e%74%42%79%49%64%28%27%6d%61%69%6c%5f%74%6f%2d%71%31%31%7a%30%64%63%75%27%29%3b%76%61%72%20%61%20%3d%20%64%6f%63%75%6d%65%6e%74%2e%63%72%65%61%74%65%45%6c%65%6d%65%6e%74%28%27%61%27%29%3b%61%2e%73%65%74%41%74%74%72%69%62%75%74%65%28%27%68%72%65%66%27%2c%20%27%6d%61%69%6c%74%6f%3a%6b%6f%6e%74%61%6b%74%40%63%61%6c%6c%6f%6e%65%2e%64%65%27%29%3b%61%2e%61%70%70%65%6e%64%43%68%69%6c%64%28%64%6f%63%75%6d%65%6e%74%2e%63%72%65%61%74%65%54%65%78%74%4e%6f%64%65%28%27%6b%6f%6e%74%61%6b%74%40%63%61%6c%6c%6f%6e%65%2e%64%65%27%29%29%3b%73%63%72%69%70%74%2e%70%61%72%65%6e%74%4e%6f%64%65%2e%69%6e%73%65%72%74%42%65%66%6f%72%65%28%61%2c%73%63%72%69%70%74%29%3b'))</script>.
+      Das Widerspruchsrecht kann formlos ausgeübt werden, etwa per Post an CallOne GmbH, Dorfstraße 60, 15806 Zossen oder per E-Mail an <script id="mail_to-q11z0dcu">eval(decodeURIComponent('%76%61%72%20%73%63%72%69%70%74%20%3d%20%64%6f%63%75%6d%65%6e%74%2e%67%65%74%45%6c%65%6d%65%6e%74%42%79%49%64%28%27%6d%61%69%6c%5f%74%6f%2d%71%31%31%7a%30%64%63%75%27%29%3b%76%61%72%20%61%20%3d%20%64%6f%63%75%6d%65%6e%74%2e%63%72%65%61%74%65%45%6c%65%6d%65%6e%74%28%27%61%27%29%3b%61%2e%73%65%74%41%74%74%72%69%62%75%74%65%28%27%68%72%65%66%27%2c%20%27%6d%61%69%6c%74%6f%3a%6b%6f%6e%74%61%6b%74%40%63%61%6c%6c%6f%6e%65%2e%64%65%27%29%3b%61%2e%61%70%70%65%6e%64%43%68%69%6c%64%28%64%6f%63%75%6d%65%6e%74%2e%63%72%65%61%74%65%54%65%78%74%4e%6f%64%65%28%27%6b%6f%6e%74%61%6b%74%40%63%61%6c%6c%6f%6e%65%2e%64%65%27%29%29%3b%73%63%72%69%70%74%2e%70%61%72%65%6e%74%4e%6f%64%65%2e%69%6e%73%65%72%74%42%65%66%6f%72%65%28%61%2c%73%63%72%69%70%74%29%3b'))</script>.
     </p>
 
 
@@ -237,8 +237,8 @@
     </p>
     <p>
       CallOne GmbH<br/>
-      Friedrich-Koenig-Straße 25a<br/>
-      55129 Mainz<br/>
+      Dorfstraße 60<br/>
+      15806 Zossen<br/>
       E-Mail: <script id="mail_to-lgqv9hfv">eval(decodeURIComponent('%76%61%72%20%73%63%72%69%70%74%20%3d%20%64%6f%63%75%6d%65%6e%74%2e%67%65%74%45%6c%65%6d%65%6e%74%42%79%49%64%28%27%6d%61%69%6c%5f%74%6f%2d%6c%67%71%76%39%68%66%76%27%29%3b%76%61%72%20%61%20%3d%20%64%6f%63%75%6d%65%6e%74%2e%63%72%65%61%74%65%45%6c%65%6d%65%6e%74%28%27%61%27%29%3b%61%2e%73%65%74%41%74%74%72%69%62%75%74%65%28%27%68%72%65%66%27%2c%20%27%6d%61%69%6c%74%6f%3a%6b%6f%6e%74%61%6b%74%40%63%61%6c%6c%6f%6e%65%2e%64%65%27%29%3b%61%2e%61%70%70%65%6e%64%43%68%69%6c%64%28%64%6f%63%75%6d%65%6e%74%2e%63%72%65%61%74%65%54%65%78%74%4e%6f%64%65%28%27%6b%6f%6e%74%61%6b%74%40%63%61%6c%6c%6f%6e%65%2e%64%65%27%29%29%3b%73%63%72%69%70%74%2e%70%61%72%65%6e%74%4e%6f%64%65%2e%69%6e%73%65%72%74%42%65%66%6f%72%65%28%61%2c%73%63%72%69%70%74%29%3b'))</script>
     </p>
     <p>
@@ -255,20 +255,19 @@
       Die für uns zuständige Datenschutzaufsichtsbehörde, bei der eine Beschwerde über eine Verletzung von Datenschutzrecht eingereicht werden kann, ist:
     </p>
     <p>
-      Landesbeauftragte für den Datenschutz und die Informationsfreiheit Rheinland-Pfalz<br/>
-      Hintere Bleiche 34<br/>
-      55116 Mainz
+      Die Landesbeauftragte für den Datenschutz und für das Recht auf Akteneinsicht<br/>
+      Stahnsdorfer Damm 77<br/>
+      14532 Kleinmachnow
     </p>
     <p>
-      Telefon: <a href="tel:+493092033500">+49 (0) 30-920 33 500</a><br/>
-      Telefax: <a href="tel:+4961312082497">+49 (0) 6131 208-2497</a><br/>
-      Webseite: <a href="https://www.datenschutz.rlp.de/">www.datenschutz.rlp.de</a><br/>
-      E-Mail: <a href="mailto: poststelle@datenschutz.rlp.de">poststelle@datenschutz.rlp.de</a>
+      Telefon: <a href="tel:+49332033560">+49 (0) 33203 356 - 0</a><br/>
+      Telefax: <a href="tel:+493320335649">+49 (0) 33203 356 - 49</a><br/>
+      E-Mail: <a href="mailto: Poststelle@LDA.Brandenburg.de">Poststelle@LDA.Brandenburg.de</a>
     </p>
 
     <h2>19. Aktualisierung dieser Datenschutzhinweise</h2>
     <p>
-      Von Zeit zu Zeit ist es erforderlich, den Inhalt der vorliegenden Datenschutzhinweise anzupassen. Wir behalten uns daher vor, diese jederzeit zu ändern. Wir werden die geänderte Version der Datenschutzhinweise registrierten Nutzern vor Inkrafttreten per E-Mail  übermitteln und an derselben Stelle wie die vorliegenden Datenschutzhinweise veröffentlichen.
+      Von Zeit zu Zeit ist es erforderlich, den Inhalt der vorliegenden Datenschutzhinweise anzupassen. Wir behalten uns daher vor, diese jederzeit zu ändern. Wir werden die geänderte Version der Datenschutzhinweise registrierten Nutzern vor Inkrafttreten per E-Mail übermitteln und an derselben Stelle wie die vorliegenden Datenschutzhinweise veröffentlichen.
     </p>
   </div>
 </div>
