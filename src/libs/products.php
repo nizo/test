48961e1d63fe449560459508c0b1d62504e29645
brian <brian.miculcy@callone.de>
2021-08-30 18:34:36 +0200

    use fixed value for rating

--- src/libs/products.php
+++ src/libs/products.php
@@ -42,75 +42,75 @@ class product
 
 	function reviews_load ()
 	{
-		$review = new review ('2021-06-08', null, 'Юсуп Адамович', 5);
+		$review = new review ('2021-06-08', null, 'Юсуп Адамович');
 		$review->text_set ('👍👍👍👍👍');
 		$this->review_add ($review);
 
-		$review = new review ('2021-01-10', 'BUNZL Healthcare', null, 5);
+		$review = new review ('2021-01-10', 'BUNZL Healthcare', null);
 		$review->text_set ('Mit CallOne als Partner an unserer Seite, konnten wir eine durchdachte und clevere digitale Telefonie etablieren. Die Selbstadministration der Telefonie gepaart mit Unterstützung aus dem CallOne-Projektmanagement, versetzt uns in die Lage unentwegt und eigenständig Anpassungen vorzunehmen und flexibel auf spontane Situationen zu reagieren. Die Möglichkeiten der statistischen Auswertbarkeit sind extrem tief und für viele Unternehmenszwecke nützlich. Tolle Basis zur Weiterentwicklung des Unternehmens. Intuitiv, logisch und smart.');
 		$this->review_add ($review);
 
-		$review = new review ('2019-05-01', 'Snipes', 'Dennis Voth', 5);
+		$review = new review ('2019-05-01', 'Snipes', 'Dennis Voth');
 		$review->text_set ('Mit CallOne können wir unseren europaweiten Kundenservice an zentraler Stelle schnell und einfach steuern und mit Hilfe von zahlreichen Statistiken analysieren und optimieren.');
 		$this->review_add ($review);
 
-		$review = new review ('2019-04-04', 'Check24', 'Dr. Rainer Klipp', 5);
+		$review = new review ('2019-04-04', 'Check24', 'Dr. Rainer Klipp');
 		$review->text_set ('Für uns als Marktführer im Bereich Preisvergleich ist eine ausgewachsene Callcenter-Software, wie sie CallOne anbietet unabdingbar für unser weiteres schnelles Wachstum.');
 		$this->review_add ($review);
 		
-		$review = new review ('2019-03-27', 'apo-discounter.de', 'Dennis Voth', 5);
+		$review = new review ('2019-03-27', 'apo-discounter.de', 'Dennis Voth');
 		$review->text_set ('Wir setzen in allen Bereichen auf Schnelligkeit, Kompetenz und modernste Technik. So können wir unsere Kunden von unserem Service begeistern und genauso hat uns auch CallOne begeistert!');
 		$this->review_add ($review);
 
-		$review = new review ('2016-07-11', null, 'Sebastian Merkhoffer', 5);
+		$review = new review ('2016-07-11', null, 'Sebastian Merkhoffer');
 		$review->text_set ('Alles top!');
 		$this->review_add ($review);
 
-		$review = new review ('2016-02-13', null, 'Babett Schultze', 5);
+		$review = new review ('2016-02-13', null, 'Babett Schultze');
 		$review->text_set ('Hervorragender Kontakt und Betreuung. Super Lösung für Kundenservice mit rundum-Features und persönlichen Einstellungen für die genauen Vorstellungen einer jeden Firma anstatt Standartpakete. Wenn es mal Rückfragen oder Wünsche zu veränderungen der Einstellungen gab, wurde diese innerhalb einer Stunde gelöst und übernommen, besser geht es nicht. Alle Einstellungen kann der Admin aber auch selber verwalten und den eigenen Firmenbedürfnissen anpassen. Super Produkt und exzellenter Service. Ich bin überdurchschnittlich zufrieden.');
 		$this->review_add ($review);
 
-		$review = new review ('2015-09-17', null, 'Sylvia Hilscher', 5);
+		$review = new review ('2015-09-17', null, 'Sylvia Hilscher');
 		$review->text_set ('Perfekte Service Center Lösung. Die Benutzeroberfläche ist angenehm und leicht zu bedienen. Besonders im Bereich Statistiken werden umfassende Möglichkeiten zur Verfügung gestellt. Zudem wird ein sehr guter Kundenservice geboten. Es findet eine persönliche und sehr kompetente Betreuung statt. Es werden viele verschiedene Serviceleistungen angeboten und unsere Anforderungen werden umgehend umgesetzt. Nur zu empfehlen!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-08-20', 'navabi', 'Leonie Theißen', 5);
+		$review = new review ('2015-08-20', 'navabi', 'Leonie Theißen');
 		$review->text_set ('Seit ein paar Monaten läuft unser gesamter telefonischer Kundensupport über CallOne. Uns war in erster Linie wichtig, eine Lösung zu erhalten, die beim täglichen Arbeiten intuitiv zu bedienen ist und speziell auf unsere Bedürfnisse zugeschnitten – und genau das haben wir erhalten. Unser persönlicher Ansprechpartner geht auf alle Anfragen sofort und individuell ein und findet immer die bestmögliche Lösung für uns.');
 		$this->review_add ($review);
 
-		$review = new review ('2015-08-16', 'TCI Rechtsanwälte', 'Stephan Schmidt', 5);
+		$review = new review ('2015-08-16', 'TCI Rechtsanwälte', 'Stephan Schmidt');
 		$review->text_set ('Wir vertrauen der CallOne bereits seit längerer Zeit in Sachen Telefonanlage, Konferenzrufnummern und allem, was zu moderner Kommunikation dazu gehört. Die Mitarbeiter reagieren auch auf ungewöhnliche Anforderung sehr schnell und finden unkompliziert Lösungen. So wünscht man sich das von seinem Dienstleister.');
 		$this->review_add ($review);
 
-		$review = new review ('2015-07-30', 'Flaconi GmbH', null, 5);
+		$review = new review ('2015-07-30', 'Flaconi GmbH', null);
 		$review->text_set ('Wir arbeiten seit über einem Jahr mit CallOne zusammen und sind sehr zufrieden. Wir haben seit Beginn der Zusammenarbeit einen Ansprechpartner, der umgehend, zuverlässig und freundlich auf unsere Anliegen eingeht. Auch wurden wir von Anfang an sehr gut beraten, um unseren Service optimieren zu können. Deshalb bekommt CallOne unsere uneingeschränkte Empfehlung!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-07-12', null, 'Felix Schmidt', 5);
+		$review = new review ('2015-07-12', null, 'Felix Schmidt');
 		$review->text_set ('CallOne zu integrieren war die absolut richtige Entscheidung. Die CallCenter-Lösung hat es geschafft unseren Kundendienst auf ein ganz neues Level zu heben und Prozesse stark zu vereinfach. Das CallOne-Team macht einen super Job in der Beratung und Betreuung. Vielen Dank!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-06-29', null, 'Ingo Vigneron', 5);
+		$review = new review ('2015-06-29', null, 'Ingo Vigneron');
 		$review->text_set ('Wir haben CallOne in diesem Jahr neu für uns entdeckt. Nach einem ersten gemeinsamen Projekt haben wir unsere Callcenter Dienste für drei Länder auf CallOne umgestellt. Weitere werden folgen, denn wir haben einen persönlichen Ansprechpartner der konsequent und flexibel auf unseren Bedarf eingeht. Für uns ist das ein großer Vorteil. Vom Home-Office bis zum Hauptstandort können wir europaweit alles zentral steuern und auswerten, natürlich rund um die Uhr und auch von Unterwegs. Danke für die gute Zusammenarbeit!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-06-18', null, 'Holger Lerch', 5);
+		$review = new review ('2015-06-18', null, 'Holger Lerch');
 		$review->text_set ('.....vom ersten Telefonat bis zur Einrichtung bei uns: EIN Ansprechpartner, der sogar meine persönlichen Einstellungen begleitet hat. Komplett runde Lösung - gerade beim Support. Vielen Dank!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-06-02', null, 'Tim Vogt', 5);
+		$review = new review ('2015-06-02', null, 'Tim Vogt');
 		$review->text_set ('CallOne verfügt über ein engagiertes und kompetentes Team mit großer Expertise im Bereich virtuelle Telefonanlagen, Anrufsteuerung und Mehrwertdienste - Tolle Zusammenarbeit, die Spaß macht. Jederzeit gerne wieder.');
 		$this->review_add ($review);
 
-		$review = new review ('2015-05-25', null, 'Tu-Binh Dang', 5);
+		$review = new review ('2015-05-25', null, 'Tu-Binh Dang');
 		$review->text_set ('Mit Hilfe von dem engagierten und kompetenten CallOne Team (Service und Technik) wurden Lösungen schnell gefunden und erfolgreich umgesetzt. Vielen Dank hier nochmal für die tolle Zusammenarbeit! Fazit: Sehr gute CallCenter-Lösung + super Kundenbetreuung. 100% Weiterempfehlung!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-04-20', null, 'Arek Radziejewski', 5);
+		$review = new review ('2015-04-20', null, 'Arek Radziejewski');
 		$review->text_set ('Optimale CallCenter-Lösung! Int. Hotline-Nr.-Auswahl, Admin-Bereich selbsterklärend und intuitive Bedienung, weltweit 24/7 online verfügbare Funktionen! Top-Service+ Beratung und transparente Kostenkontrolle! Sehr Empfehlenswert! Individuelle Bespieglung der Warteschleife (wir haben eine Radiospot als mp3 eingebunden). Dank CallOne konnten wir eine int. Kampagne reibungslos umsetzen und unsere Hotliner waren für die Endkunden optimal verfügbar/ erreichbar! Danke CallOne!');
 		$this->review_add ($review);
 
-		$review = new review ('2015-03-27', null, 'Wytze Kempenaar', 5);
+		$review = new review ('2015-03-27', null, 'Wytze Kempenaar');
 		$review->text_set ('Komplette Lösung. Top Service! Innovativ!');
 		$this->review_add ($review);
 	}
@@ -139,8 +139,8 @@ class product
 
 		$aggregate_rating = [];
 		$aggregate_rating['@type']			= 'AggregateRating';
-		$aggregate_rating['ratingValue'] 	= (empty ($this->reviews_get()) ? 0 : 5);
-		$aggregate_rating['reviewCount']	= count ($this->reviews_get());
+		$aggregate_rating['ratingValue'] 	= 5;
+		$aggregate_rating['reviewCount']	= (count ($this->reviews_get()) ? count ($this->reviews_get()) : 1);
 		$data['aggregateRating'] = $aggregate_rating;
 
 		if (!empty ($this->reviews_get()))
@@ -175,7 +175,7 @@ class product
 
 				$review_rating = [];
 				$review_rating['@type'] = 'Rating';
-				$review_rating['ratingValue'] = $review_tmp->rating_get ();
+				$review_rating['ratingValue'] = 5;
 				$review['reviewRating'] = $review_rating;
 
 				$reviews[] = $review;
@@ -200,15 +200,13 @@ class review
 	private $date;
 	private $company;
 	private $author;
-	private $rating;
 	private $text;
 
-	function __construct ($date, $company, $author, $rating)
+	function __construct ($date, $company, $author)
 	{
 		$this->date		= $date;
 		$this->company	= $company;
 		$this->author	= $author;
-		$this->rating	= $rating;
 		$this->text		= null;
 	}
 
@@ -222,10 +220,6 @@ class review
 
 	function author_get ()
 	{	return $this->author;
-	}
-
-	function rating_get ()
-	{	return $this->rating;
 	}
 
 	function text_set ($text)
