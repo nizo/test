ce48088961fe39550d286f1abd9d1d792c45109e
brian <brian.miculcy@callone.de>
2021-09-01 16:43:55 +0200

    don't mix indentation tabs with spaces use self descriptive variable names

--- src/libs/faqs.php
+++ src/libs/faqs.php
@@ -1,38 +1,38 @@
 <?php
 class faqs
 {
-    private $list;
+	private $list;
 
 	function __construct ()
 	{
-        $this->list = [];
+		$this->list = [];
 	}
 
-    function faq_add ($q, $a)
-    {
-        $this->list[] = new faq_entry ($q, $a);
-    }
+	function faq_add ($question, $answer)
+	{
+		$this->list[] = new faq_entry ($question, $answer);
+	}
 
-    function structured_data_get_raw ()
+	function structured_data_get_raw ()
 	{
 		$data = [];
-        
+		
 		$data['@context']		= 'http://schema.org/';
 		$data['@type']			= 'FAQPage';
 
 		$main_entity = [];
 		
-        foreach ($this->list as $faq)
-        {
-            $entry = [];
-            $entry['@type'] = 'Question';
-            $entry['name'] = $faq->question_get ();
-            $accepted_answer = [];
-            $accepted_answer['@type'] = 'Answer';
-            $accepted_answer['text'] = $faq->answer_get ();
-            $entry['acceptedAnswer'] = $accepted_answer;
-            $main_entity[] = $entry;
-        }
+		foreach ($this->list as $faq)
+		{
+			$entry = [];
+			$entry['@type'] = 'Question';
+			$entry['name'] = $faq->question_get ();
+			$accepted_answer = [];
+			$accepted_answer['@type'] = 'Answer';
+			$accepted_answer['text'] = $faq->answer_get ();
+			$entry['acceptedAnswer'] = $accepted_answer;
+			$main_entity[] = $entry;
+		}
 
 		$data['mainEntity'] = $main_entity;
 
@@ -46,52 +46,52 @@ class faqs
 		echo '</script>';
 	}
 
-    function html_get_code ($extra_classes = '')
-    {
-        $html = '';
+	function html_get_code ($extra_classes = '')
+	{
+		$html = '';
 
-        foreach ($this->list as $faq)
-        {
-            $html .= '<div class="toggle-box '.$extra_classes.'">';
-            $html .= '<input type="checkbox" id="'.md5 ($faq->question_get ()).'" />';
-            $html .= '<label for="'.md5 ($faq->question_get ()).'">'.$faq->question_get ().'</label>';
-            $html .= '<div class="toggle-box__content">'.$faq->answer_get ().'</div>';
-            $html .= '</div>';
-        }
+		foreach ($this->list as $faq)
+		{
+			$html .= '<div class="toggle-box '.$extra_classes.'">';
+			$html .= '<input type="checkbox" id="'.md5 ($faq->question_get ()).'" />';
+			$html .= '<label for="'.md5 ($faq->question_get ()).'">'.$faq->question_get ().'</label>';
+			$html .= '<div class="toggle-box__content">'.$faq->answer_get ().'</div>';
+			$html .= '</div>';
+		}
 
-        echo $html;
-    }
+		echo $html;
+	}
 }
 
 class faq_entry
 {
-    private $question;
-    private $answer;
+	private $question;
+	private $answer;
 
-    function __construct ($question, $answer)
-    {
-        $this->question_set ($question);
-        $this->answer_set ($answer);
-    }
+	function __construct ($question, $answer)
+	{
+		$this->question_set ($question);
+		$this->answer_set ($answer);
+	}
 
-    function question_set ($question)
-    {
-        $this->question = $question;
-    }
+	function question_set ($question)
+	{
+		$this->question = $question;
+	}
 
-    function question_get ()
-    {
-        return $this->question;
-    }
+	function question_get ()
+	{
+		return $this->question;
+	}
 
-    function answer_set ($answer)
-    {
-        $this->answer = $answer;
-    }
+	function answer_set ($answer)
+	{
+		$this->answer = $answer;
+	}
 
-    function answer_get ()
-    {
-        return $this->answer;
-    }
+	function answer_get ()
+	{
+		return $this->answer;
+	}
 }
 ?>
