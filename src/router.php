dd35ea5287a97ef10c80ddcc22e71b69c61428f5
Max Boll <max.boll@callone.de>
2021-10-13 17:51:28 +0200

    Added more content to new home page Added icons page to get an overview of all svg icons

--- src/router.php
+++ src/router.php
@@ -65,6 +65,15 @@ $r->addRoute('GET', '/components', function() {
   require_once('./views/de/components.php');
   require_once('./layouts/footer-components.php');
 });
+$r->addRoute('GET', '/icons', function() {
+  $title = 'Icons';
+  $description = '';
+  $keywords = '';
+  $background = 'components--scrollbar';
+  require_once('./layouts/header-components.php');
+  require_once('./views/de/icons.php');
+  require_once('./layouts/footer-components.php');
+});
 
 $r->addRoute('GET', '/components_overview', function() {
   $title = 'components_overview';
--- src/views/de/index.php
+++ src/views/de/index.php
@@ -12,12 +12,12 @@
             </p>
         </div>
         <div class="hero__image">
-            <!-- <img src="/assets/images/illus/startpage-header-lotta-jamie-illustration.png" alt="" /> -->
+            
         </div>
     </header>
 </div>
 
-<div class="section section--light-green-white-2">
+<div class="section section--light-green-white-2 section--no-padding-bottom">
     <div class="anchors">
         <div class="anchors__title">
             <strong>Schnelleinstieg:</strong> Für was interessieren Sie sich?
@@ -52,7 +52,22 @@
             Überdurchschnittliche Kundenerlebnisse!
         </p>
 
-        <p class="centered">TODO: Small Tabs with Client Logos</p>
+        <div class="small-tabs">
+            <div class="small-tabs__tabs">
+                <div class="small-tabs__tab" data-tab="1">Enterprise</div>
+                <div class="small-tabs__tab" data-tab="2">Mittelstand</div>
+                <div class="small-tabs__tab" data-tab="3">Fast-Growing</div>
+            </div>
+            <div class="small-tabs__content centered" data-tab="1">
+                <img src="/assets/images/client-logos/Enterprise-Kombi.svg" alt="" />
+            </div>
+            <div class="small-tabs__content centered" data-tab="2">
+                <img src="/assets/images/client-logos/Mittelstand-Kombi.svg" alt="" />
+            </div>
+            <div class="small-tabs__content centered" data-tab="3">
+                <img src="/assets/images/client-logos/FastGrowing-Kombi.svg" alt="" />
+            </div>
+        </div>
 
         <div class="tablist">
             <div class="tablist__links tablist__links--full-width">
@@ -124,14 +139,21 @@
             </div>
 
             <div class="tablist__content paper" data-tab="2">
-                <div class="paper__row">
+                <div class="paper__row" style="background-image: url(/assets/images/illus/ROI-airdrop-illustration.svg);
+                                                background-repeat: no-repeat;
+                                                background-position: 82% bottom;
+                                                background-size: 35%;">
                     <div class="paper__item co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
                         <div class="co-grid__col co-grid__col--6-xs">
+                            <br />
+                            <br />
                             <h3>Unsere Telefonanlage verdient für Sie Geld und reduziert Ihre Kosten</h3>
                             <p class="bigtext">Wir steigern nachweislich Ihren ROI, indem wir gemeinsam an den großen Stellschrauben im Kundenservice drehen.</p>
                             <p>
                                 <a href="#todo" class="btn btn--primary">Mehr erfahren</a>
                             </p>
+                            <br />
+                            <br />
                         </div>
                     </div>
                 </div>
@@ -152,11 +174,11 @@
                                 </div>
                                 <div class="carddeck__card">
                                     <h4>Lorem, ipsum dolor.</h4>
-                                    <p class="bigtext">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit quaerat laudantium recusandae asperiores sapiente, eaque repellendus, vitae, officiis excepturi debitis ea ipsum sit sunt. Laborum consequatur eligendi eveniet obcaecati pariatur!</p>
+                                    <p class="bigtext">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit quaerat laudantium recusandae asperiores sapiente, eaque repellendus, vitae, officiis excepturi.</p>
                                 </div>
                                 <div class="carddeck__card">
                                     <h4>Lorem ipsum dolor sit.</h4>
-                                    <p class="bigtext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas unde ea placeat et officiis assumenda modi eaque, esse aut quisquam delectus magnam distinctio facere voluptatem! Illo, nulla debitis. Facilis, dolorem.</p>
+                                    <p class="bigtext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas unde ea placeat et officiis assumenda modi eaque, esse aut quisquam delectus magnam distinctio facere.</p>
                                 </div>
                             </div>
                         </div>
@@ -165,7 +187,150 @@
             </div>
 
             <div class="tablist__content paper" data-tab="3">
-                Content 3
+                <div class="paper__row" style="background-image: url(/assets/images/illus/client-diversity-illustration.svg);
+                                                background-repeat: no-repeat;
+                                                background-position: right bottom;
+                                                background-size: 50%;">
+                    <div class="paper__item co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                        <div class="co-grid__col co-grid__col--6-xs">
+                            <br />
+                            <br />
+                            <h3>Eine Plattform, unendlich Optionen</h3>
+                            <p class="bigtext">Maximale Flexibilität wie Sie und wo Sie mit uns telefonieren. Ob Hardware, Browser oder mobil.</p>
+                            <p>
+                                <a href="#todo" class="btn btn--primary">Mehr erfahren</a>
+                            </p>
+                            <br />
+                            <br />
+                        </div>
+                    </div>
+                </div>
+                <div class="paper__row">
+                    <div class="paper__item co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                        <div class="co-grid__col co-grid__col--8-xs co-grid__col--vcentered">
+                            <?php require_once('assets/images/illus/clients-circle-callcenter-software.svg'); ?>
+                            <script>
+                                let svg = document.querySelector('.clients-circle-callcenter-software');
+                                let circles = svg.querySelectorAll('.circle');
+                                let circleIcons = svg.querySelectorAll('.circle-icon');
+                                let currentCircle = 0;
+                                let countCircles = circles.length;
+
+                                setInterval(e => {
+                                    circles.forEach(circle => {
+                                        circle.setAttribute('stroke', 'black');
+                                        circle.setAttribute('stroke-width', '2');
+                                        circle.setAttribute('stroke-opacity', '0.1');
+                                    });
+                                    circleIcons.forEach(circleIcon => circleIcon.setAttribute('opacity', '0.5'));
+                                    currentCircle++;
+                                    if (currentCircle >= countCircles)
+                                        currentCircle = 0;
+                                    circles[currentCircle].setAttribute('stroke', '#86ed18');
+                                    circles[currentCircle].setAttribute('stroke-width', '4');
+                                    circles[currentCircle].setAttribute('stroke-opacity', '1');
+                                    circleIcons[currentCircle].setAttribute('opacity', '1');
+                                }, 2000);
+                            </script>
+                        </div>
+                        <div class="co-grid__col co-grid__col--4-xs co-grid__col--vcentered">
+                            <div class="left-border">
+                                <h4>Callcenter-Software</h4>
+                                <p class="bigtext">Binden Sie flexibel jedes gängige SIP-Tischtelefon oder DECT-Telefon ein und nutzen sie die Möglichkeit der bequemen Verwaltung in einer Oberfläche.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--wide">
+        <div class="paper paper--gradient">
+            <div class="paper__row" style="background-image: url(/assets/images/illus/brochure-CCS-checklist.png);
+                                            background-repeat: no-repeat;
+                                            background-size: auto 100%;
+                                            background-position: 6% center;">
+                <div class="paper__item co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                    <div class="co-grid__col co-grid__col--5-xs"></div>
+                    <div class="co-grid__col co-grid__col--7-xs co-grid__col--vcentered">
+                        <br /><br />
+                        <h3>So holt Ihr Kundenservice alles raus!</h3>
+                        <p class="bigtext">Unseres hilfreiches Handbuch mit Anforderungen, die Sie an Ihre  Callcenter Software stellen sollten.</p>
+                        <p>
+                            <a href="#todo" class="btn btn--primary">Jetzt herunterladen</a>
+                        </p>
+                        <br /><br />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--wide">
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--7-xs">
+                <h4>Callcenter Software Anbieter gibt es viele...</h4>
+                <h1>Kaum einer bietet Ihnen einen nachweislichen Return-on-Investment wie CallOne.</h1>
+
+                <p>TODO: Cards</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--black-grey section--wave-animation">
+    <div class="section__content section__content--narrow">
+        <h1 class="centered">Kundenbesessene Unternehmen <br />vertrauen auf CallOne</h1>
+        <p class="centered">Kunden kommen zu CallOne wenn sie richtig Geld sparen wollen! Wir sind fokussiert auf Ihren Return-on-Investment. Gemeinsam drehen wir an den großen Kosten- und Einsparpotenzialen.</p>
+    </div>
+    <div class="section__content section__content--wide">
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--3-xs number-card number-card--on-black">
+                <div class="number-card__number" style="--color:#86ed18;">
+                    <span class="counting-number" data-suffix="%" data-start="0" data-end="25">25%</span>
+                </div>
+                weniger Kosten pro Kontakt durch gesteigerte FCR*
+            </div>
+            <div class="co-grid__col co-grid__col--3-xs number-card number-card--on-black">
+                <div class="number-card__number" style="--color:#86ed18;">
+                    <span class="counting-number" data-start="0" data-end="5">5</span>-<span class="counting-number" data-suffix="s" data-start="0" data-end="15">15s</span>
+                </div>
+                kürzere Gesprächsdauern (AHT)*
+            </div>
+            <div class="co-grid__col co-grid__col--3-xs number-card number-card--on-black">
+                <div class="number-card__number" style="--color:#86ed18;">
+                    <span class="counting-number" data-start="0" data-end="5">5</span>-<span class="counting-number" data-suffix="s" data-start="0" data-end="15">15s</span>
+                </div>
+                kürzere Gesprächsdauern (AHT)*
+            </div>
+            <div class="co-grid__col co-grid__col--3-xs co-grid__col--vcentered">
+                <h3>Unsere Kunden erhalten Ergebnisse.</h3>
+            </div>
+        </div>
+
+        <br /><br />
+
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--7-xs">
+                <div class="co-grid">
+                    <div class="co-grid__col co-grid__col--vcentered">
+                        <img src="/assets/images/icons_svg/first-call-white.svg" alt="" width="70" />
+                    </div>
+                    <div class="co-grid__col co-grid__col--fill">
+                        <h3>
+                            Lust Ihre Einsparpotenziale anzupacken? <br />
+                            Auf ins unverbindliche Erstgespräch!
+                        </h3>
+                    </div>
+                </div>
+                <p>
+                    <a href="#" class="btn btn--primary" data-openmodal="contact-sales">Erstgespräch buchen</a>
+                </p>
             </div>
         </div>
     </div>
