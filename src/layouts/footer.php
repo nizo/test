96ee8ef5348abb59d592bccb9b76a650e4cd44de
Max Boll <max.boll@callone.de>
2021-12-13 14:25:31 +0100

    Adjusted mobile navigation and mobile footer

--- src/layouts/footer.php
+++ src/layouts/footer.php
@@ -47,10 +47,10 @@
             <p>
               <a href="/callcenter-software" title="Callcenter Software">Callcenter Software</a><br />
               <a href="/voip-telefonanlage" title="VoIP-Telefonanlage">VoIP-Telefonanlage</a><br />
-              <a href="/softphone" title="VoIP-Softphone">VoIP-Softphone</a>
               <a href="/0800-lokal-nummern" title="Rufnummern Service">Rufnummern Service</a><br />
               <a href="/call-tracking" title="Call-Tracking">Call-Tracking</a><br />
               <a href="/cti-schnittstellen" title="Callcenter-Bot">Callcenter-Bot</a>
+              <a href="/softphone" title="VoIP-Softphone">VoIP-Softphone</a>
             </p>
           </div>
           <div class="footer-column">
@@ -98,7 +98,6 @@
           </p>
           <div class="button-set centered">
             <a href="#" class="button secondary trackedElement" data-openmodal="contact-sales" data-label="Button footer - FÜR DEMO ANMELDEN​" data-category="Link" data-action="opens sales modal">FÜR DEMO ANMELDEN​</a>
-            <a href="/preise" class="button primary trackedElement" data-label="Button footer - Preise" data-category="Link" data-action="linked to /preise">Preise</a>
           </div>
         </div>
         <ul class="footer-menu">
@@ -118,6 +117,7 @@
               <li><a href="/0800-lokal-nummern" title="Rufnummern Service">Rufnummern Service</a></li>
               <li><a href="/call-tracking" title="Call-Tracking">Call-Tracking</a></li>
               <li><a href="/cti-schnittstellen" title="Callcenter-Bot">Callcenter-Bot</a></li>
+              <li><a href="/softphone" title="VoIP-Softphone">VoIP-Softphone</a></li>
             </ul>
           </li>
           <li>
