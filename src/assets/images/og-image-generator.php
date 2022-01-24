60a8dfc888fcdb2e166712978606b0204f74ce1c
Max Boll <max.boll@callone.de>
2022-01-24 16:23:52 +0100

    Firts functioning prototype of og image generator

--- src/assets/images/og-image-generator.php
+++ src/assets/images/og-image-generator.php
@@ -3,41 +3,70 @@ $name = 'CallOne';
 $width = 1200;
 $height = 630;
 
-if (isset($_GET['resolution']) && isset($_GET['name'])) {
+if (isset($_GET['resolution']) && $_GET['resolution'] != 'x') {
     $resolution = explode('x', $_GET['resolution']);
-    $name = ucwords(str_replace('_', ' ', $_GET['name']));
     $width = $resolution[0];
     $height = $resolution[1];
 }
+if (isset($_GET['name'])) {
+    $name = str_replace('__', "\n", $_GET['name']);
+    $name = str_replace('_', ' ', $name);
+    $name = ucwords(wordwrap($name, 20, "\n"));
+}
 
-header("Content-type:image/jpeg");
+// Essentials
+$font_light = '../fonts/apercu-light-pro-web/apercu-light-pro.ttf';
+$font_regular = '../fonts/apercu-regular-pro-web/apercu-regular-pro.ttf';
+$font_medium = '../fonts/apercu-medium-pro-web/apercu-medium-pro.ttf';
+$font_bold = '../fonts/Px-Grotesk-Bold/Px-Grotesk-Bold.ttf';
+$image = imagecreatefrompng('./og/bg-prepared.png');
+$font_size = 64;
+$angle = 0;
 
-$font = '../fonts/apercu-regular-pro-web/apercu-regular-pro.tff';
-$image = imagecreate($width, $height);
-$bg_image = imagecreatefromjpeg('./og/background.jpg');
-$bg_image_x = imagesx($bg_image);
-$bg_image_y = imagesy($bg_image);
-$new_x = $width;
-$new_y = $bg_image_y / $bg_image_x * $width;
+// Crop image to wanted size
+$c_width = imagesx($image);
+$c_height = imagesY($image);
+$cropped = false;
 
-imagecopyresized(
-    $image,
-    $bg_image,
-    0,
-    0,
-    0,
-    0,
-    $width,
-    $height,
-    $new_x,
-    $new_y
-);
+if ($c_width != $width || $c_height != $height) {
+    $c_ratio = $c_width / $c_height;
+    $n_ratio = $width / $height;
 
+    if ($c_ratio < $n_ratio) {
+        $ratio = $c_width / $width;
+        $n_width = $c_width;
+        $n_height = $height * $ratio;
+    } else {
+        $ratio = $c_height / $height;
+        $n_height = $c_height;
+        $n_width = $width * $ratio;
+    }
+
+    $image = imagecrop($image, [
+        'x' => 0,
+        'y' => 0, // Keep image aligned top
+        'width' => $n_width,
+        'height' => $n_height
+    ]);
+
+    $cropped = true;
+}
+
+// Colors
 $white = imagecolorallocate($image, 255, 255, 255);
-$black = imagecolorallocate($image, 0, 0, 0);
-imagettftext($image, 36, 0, 100, 100, $white, $font, $name);
-imagefilledrectangle($image, 0, 0, $width, $height, $white);
-imagefilledpolygon($image,  array(0, 0, $width, 0, 0, $height), 3, $black);
-imagejpeg($image);
+
+// Add text
+imagettftext($image, $font_size, $angle, 10, 140, $white, $font_bold, $name);
+
+// Scale down to wanted size
+if ($cropped) {
+    $image = imagescale($image, $width);
+}
+
+// Serve image
+header("Content-type:image/png");
+imagepng($image);
+
+// Destroy image
 imagedestroy($image);
 ?>
