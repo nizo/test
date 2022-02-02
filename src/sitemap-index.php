1490e6b2fe32e1c00033813a51086fd38535681a
Max Boll <max.boll@callone.de>
2022-02-02 14:18:01 +0100

    Added dynamic domain to sitemap-index

--- src/sitemap-index.php
+++ src/sitemap-index.php
@@ -1,19 +1,22 @@
 <?php
+require_once('./libs/router.inc.php');
+
 header('Content-type: text/xml');
+
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">	
    <sitemap>
-      <loc>https://www.callone.de/sitemap-website.xml</loc>
+      <loc><?= DOMAIN ?>/sitemap-website.xml</loc>
       <lastmod><?= date("Y-m-d") ?></lastmod>
    </sitemap>
    <sitemap>
-      <loc>https://www.callone.de/blog/sitemap.xml</loc>
+      <loc><?= DOMAIN ?>/blog/sitemap.xml</loc>
       <lastmod><?= date("Y-m-d") ?></lastmod>
    </sitemap>
    <sitemap>
-      <loc>https://www.callone.de/faq/sitemap.xml</loc>
+      <loc><?= DOMAIN ?>/faq/sitemap.xml</loc>
       <lastmod><?= date("Y-m-d") ?></lastmod>
    </sitemap>
 </sitemapindex>
