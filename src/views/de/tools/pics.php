5bb2ff86ca5bf235be6e65891aa5d8aeeb05f935
Max Boll <max.boll@callone.de>
2022-02-16 18:02:13 +0100

    Added darkmode and icons to pics page

--- src/views/de/tools/pics.php
+++ src/views/de/tools/pics.php
@@ -6,6 +6,9 @@ $images_folder = 'assets/images/';
 $directories = [
     'backgrounds_png' => 'Backgrounds PNG',
     'backgrounds_svg' => 'Backgrounds SVG',
+    'icons_png' => 'Icons PNG',
+    'icons_svg' => 'Icons SVG',
+    'illus' => 'Illustrationen',
     'illus' => 'Illustrationen',
     'og' => 'OpenGraph Backgrounds',
     'photos' => 'Fotos',
@@ -36,12 +39,23 @@ function get_files_recursively($dir, &$results = array()) {
 
 <style>
     .pics {
-        --size: 10%;
+        --size: 100px;
 
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         height: 100vh;
+    }
+
+    .pics--dark .pics__theme-toggle:after {
+        content: ' ausschalten';
+    }
+    .pics--dark .pic {
+        background-color: #000;
+        border-color: #333;
+    }
+    .pics--dark .pics__right {
+        background-color: #222;
     }
 
     .pics__left {
@@ -79,13 +93,25 @@ function get_files_recursively($dir, &$results = array()) {
         margin-left: 1em;
     }
 
+    .pics__theme-toggle {
+        cursor: pointer;
+        background-color: #fff;
+        border-radius: 100px;
+        padding: .5em;
+        margin-bottom: 1em;
+        font-weight: bold;
+    }
+    .pics__theme-toggle:after {
+        content: ' einschalten';
+    }
+
     .pics__right {
         flex: 1;
         display: flex;
         flex-direction: row;
         flex-wrap: wrap;
         overflow-y: auto;
-        justify-content: space-evenly;
+        justify-content: flex-start;
         align-items: flex-start;
         padding: 1em;
         background-color: #fcfcfc;
@@ -96,16 +122,15 @@ function get_files_recursively($dir, &$results = array()) {
         display: flex;
         justify-content: center;
         align-items: center;
-        flex: 0 0 calc(var(--size) - 1em);
-        aspect-ratio: 1/1;
+        width: auto;
+        height: calc(1em + var(--size));
         padding: .5em;
         border: 1px solid #eee;
         margin: .5em;
         background-color: #fff;
     }
     .pic img {
-        max-width: 100%;
-        max-height: 100%;
+        height: 100%;
     }
 </style>
 
@@ -113,7 +138,11 @@ function get_files_recursively($dir, &$results = array()) {
     <div class="pics__left">
         <div class="pics__size">
             <label for="size">Vorschaugröße</label>
-            <input type="range" name="size" id="size" value="10" min="5" max="50" />
+            <input type="range" name="size" id="size" value="100" min="50" max="300" />
+        </div>
+
+        <div class="pics__theme-toggle">
+            Dunkelmodus
         </div>
 
         <ul>
@@ -130,7 +159,7 @@ function get_files_recursively($dir, &$results = array()) {
     <div class="pics__right">
         <?php
         // Check if a specific directory ID is requested and adjust $directories variable accordingly
-        $requested_id = htmlspecialchars($_GET['id']);
+        $requested_id = $_GET['id'];
         if (array_key_exists($requested_id, $directories)) {
             $directories = [
                 $requested_id => $directories[$requested_id]
@@ -143,8 +172,8 @@ function get_files_recursively($dir, &$results = array()) {
                 $pathinfo = pathinfo($file);
                 if (is_dir($images_folder.$key.'/'.$file) || !in_array($pathinfo['extension'], $allowed_extensions))
                     continue;
-                echo '<a href="'.$file.'" class="pic" download>';
-                echo '  <img src="'.$file.'" alt="" title="'.$file.'" />';
+                echo '<a href="'.$file.'" class="pic" target="_blank" title="'.$file.'">';
+                echo '  <img src="'.$file.'" alt="" />';
                 echo '</a>';
             }
         }
@@ -158,6 +187,12 @@ function get_files_recursively($dir, &$results = array()) {
     let pics = document.querySelector('.pics');
     size_slider.addEventListener('input', e => {
         let value = e.currentTarget.value;
-        pics.style.setProperty('--size', value + '%');
+        pics.style.setProperty('--size', value + 'px');
+    });
+
+    // Toggle theme
+    let theme_button = document.querySelector('.pics__theme-toggle');
+    theme_button.addEventListener('click', e => {
+        pics.classList.toggle('pics--dark');
     });
 </script>
