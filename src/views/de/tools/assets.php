6d2ec57a0ae1c244dea886b7a97c9eaa8cf4496f
Max Boll <max.boll@callone.de>
2022-02-17 10:11:02 +0100

    Renamed /pics to /tools/assets, Added videos to assets page Improved styles on assets page Tidy up code

--- src/views/de/tools/assets.php
+++ src/views/de/tools/assets.php
@@ -1,6 +1,7 @@
 <?php
-// Images root folder
-$images_folder = 'assets/images/';
+// Root folders
+$folder_images = 'assets/images/';
+$folder_videos = 'assets/videos/';
 
 // Directories to scan (sub-directories included) + Readable name for display
 $directories = [
@@ -17,7 +18,8 @@ $directories = [
 ];
 
 // Only these file extensionsa re going to be displayed
-$allowed_extensions = ['jpg', 'svg', 'png', 'jpeg', 'gif'];
+$allowed_extensions = ['jpg', 'svg', 'png', 'jpeg', 'gif', 'mp4', 'webm'];
+$video_extensions = ['webm', 'mp4'];
 
 // Get files recursively from given path
 function get_files_recursively($dir, &$results = array()) {
@@ -38,47 +40,67 @@ function get_files_recursively($dir, &$results = array()) {
 ?>
 
 <style>
-    .pics {
+    .assets {
         --size: 100px;
 
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         height: 100vh;
+        background-color: #fcfcfc;
     }
 
-    .pics--dark .pics__theme-toggle:after {
+    .assets--dark .assets__theme-toggle:after {
         content: ' ausschalten';
     }
-    .pics--dark .pic {
+    .assets--dark .asset {
         background-color: #000;
         border-color: #333;
     }
-    .pics--dark .pics__right {
+    .assets--dark {
         background-color: #222;
     }
 
-    .pics__left {
+    .assets__left {
+        overflow-y: auto;
         flex: 0 0 300px;
         background-color: #edf2e2;
         padding: 1em;
     }
 
-    .pics__left ul {
+    .assets__left ul {
         list-style: none;
     }
 
-    .pics__left ul li + li {
+    .assets__left ul li + li {
         border-top: 2px solid rgba(0, 0, 0, 0.1);
     }
 
-    .pics__left ul li a {
+    .assets__left ul li a {
         display: block;
         padding: .5em 0;
         color: #000;
     }
 
-    .pics__size {
+    .assets__left, .assets__right {
+        --scrollbarBG: transparent;
+        --thumbBG: #ccc;
+        scrollbar-width: thin;
+        scrollbar-color: var(--thumbBG) var(--scrollbarBG);
+    }
+    .assets__left::-webkit-scrollbar, .assets__right::-webkit-scrollbar {
+        width: 6px;
+    }
+    .assets__left::-webkit-scrollbar-track, .assets__right::-webkit-scrollbar-track {
+        background: var(--scrollbarBG);
+    }
+    .assets__left::-webkit-scrollbar-thumb, .assets__right::-webkit-scrollbar-thumb {
+        background-color: var(--thumbBG) ;
+        border-radius: 6px;
+        border: 3px solid var(--scrollbarBG);
+    }
+
+    .assets__size {
         font-weight: bold;
         display: flex;
         align-items: center;
@@ -88,12 +110,13 @@ function get_files_recursively($dir, &$results = array()) {
         margin-bottom: 1em;
     }
 
-    .pics__size input {
+    .assets__size input {
         flex: 1;
+        width: 100%;
         margin-left: 1em;
     }
 
-    .pics__theme-toggle {
+    .assets__theme-toggle {
         cursor: pointer;
         background-color: #fff;
         border-radius: 100px;
@@ -101,11 +124,11 @@ function get_files_recursively($dir, &$results = array()) {
         margin-bottom: 1em;
         font-weight: bold;
     }
-    .pics__theme-toggle:after {
+    .assets__theme-toggle:after {
         content: ' einschalten';
     }
 
-    .pics__right {
+    .assets__right {
         flex: 1;
         display: flex;
         flex-direction: row;
@@ -114,10 +137,11 @@ function get_files_recursively($dir, &$results = array()) {
         justify-content: flex-start;
         align-items: flex-start;
         padding: 1em;
-        background-color: #fcfcfc;
+        align-self: flex-start;
+        max-height: 100vh;
     }
 
-    .pic {
+    .asset {
         position: relative;
         display: flex;
         justify-content: center;
@@ -129,53 +153,70 @@ function get_files_recursively($dir, &$results = array()) {
         margin: .5em;
         background-color: #fff;
     }
-    .pic img {
+    .asset img {
+        height: 100%;
+    }
+    .asset video {
         height: 100%;
     }
 </style>
 
-<div class="pics">
-    <div class="pics__left">
-        <div class="pics__size">
+<div class="assets">
+    <div class="assets__left">
+        <div class="assets__size">
             <label for="size">Vorschaugröße</label>
             <input type="range" name="size" id="size" value="100" min="50" max="300" />
         </div>
 
-        <div class="pics__theme-toggle">
+        <div class="assets__theme-toggle">
             Dunkelmodus
         </div>
 
         <ul>
-            <li><a href="/pics">Alle Bilder anzeigen</a></li>
+            <li><a href="/tools/assets">Alles anzeigen</a></li>
             <?php
             // List available directories
             foreach ($directories as $key => $value) {
-                echo '<li><a href="/pics?id='.$key.'">'.$value.'</a></li>';
+                echo '<li><a href="/tools/assets?id='.$key.'">'.$value.'</a></li>';
             }
             ?>
+            <li><a href="/tools/assets?id=videos">Videos</a></li>
         </ul>
     </div>
 
-    <div class="pics__right">
+    <div class="assets__right">
         <?php
         // Check if a specific directory ID is requested and adjust $directories variable accordingly
         $requested_id = $_GET['id'];
+        $show_videos = false;
+        
+        // Prepopulate files with all images/videos
+        $files = array_merge(
+            get_files_recursively($folder_images),
+            get_files_recursively($folder_videos)
+        );
+
         if (array_key_exists($requested_id, $directories)) {
-            $directories = [
-                $requested_id => $directories[$requested_id]
-            ];
+            $files = get_files_recursively($folder_images.$requested_id);
+        } else if ($requested_id == 'videos') {
+            $files = get_files_recursively($folder_videos);
         }
 
-        foreach ($directories as $key => $value) {
-            $files = get_files_recursively($images_folder.$key);
-            foreach ($files as $file) {
-                $pathinfo = pathinfo($file);
-                if (is_dir($images_folder.$key.'/'.$file) || !in_array($pathinfo['extension'], $allowed_extensions))
-                    continue;
-                echo '<a href="'.$file.'" class="pic" target="_blank" title="'.$file.'">';
-                echo '  <img src="'.$file.'" alt="" />';
-                echo '</a>';
+        foreach ($files as $file) {
+            $pathinfo = pathinfo($file);
+            $file_directory = $pathinfo['dirname'];
+            $file_name = $pathinfo['basename'];
+            $file_extension = $pathinfo['extension'];
+
+            if (is_dir($file) || !in_array($file_extension, $allowed_extensions))
+                continue;
+            echo '<a href="'.$file.'" class="asset" target="_blank" title="'.$file.'">';
+            if (in_array($file_extension, $video_extensions)) {
+                echo '<video src="/'.$file.'" controls></video>';
+            } else {
+                echo '<img src="/'.$file.'" alt="" />';
             }
+            echo '</a>';
         }
         ?>
     </div>
@@ -184,15 +225,15 @@ function get_files_recursively($dir, &$results = array()) {
 <script>
     // Change image preview size when slider is moved
     let size_slider = document.querySelector('input#size');
-    let pics = document.querySelector('.pics');
+    let assets = document.querySelector('.assets');
     size_slider.addEventListener('input', e => {
         let value = e.currentTarget.value;
-        pics.style.setProperty('--size', value + 'px');
+        assets.style.setProperty('--size', value + 'px');
     });
 
     // Toggle theme
-    let theme_button = document.querySelector('.pics__theme-toggle');
+    let theme_button = document.querySelector('.assets__theme-toggle');
     theme_button.addEventListener('click', e => {
-        pics.classList.toggle('pics--dark');
+        assets.classList.toggle('assets--dark');
     });
 </script>
