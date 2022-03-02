460f05ef4acdd8429e88d3bfc84982457bca5b1e
brian <brian.miculcy@callone.de>
2022-03-02 17:32:48 +0100

    change text

--- src/partials/banderole.php
+++ src/partials/banderole.php
@@ -1,5 +1,5 @@
 <div class="banderole">
-    <div class="banderole__text">Webinar 10.03.22 - 10 Booster für den Kundensupport</div><a href="https://www.linkedin.com/events/serviceexzellenz-10boosterf-rih6903034712382935040" target="_blank" rel="nofollow">Mehr erfahren</a>
+    <div class="banderole__text">Booster für Ihren Kundenservice. Kostenfreies Webinar.</div><a href="https://www.linkedin.com/events/serviceexzellenz-10boosterf-rih6903034712382935040" target="_blank" rel="nofollow">JETZT TEILNEHMEN</a>
 </div>
 
 <div class="banderole-spacer"></div>
