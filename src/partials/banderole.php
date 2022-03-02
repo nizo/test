1e7fd9bd866c7401f8a327a894292fa767f5867e
Max Boll <max.boll@callone.de>
2022-03-02 11:50:42 +0100

    Banderole Adjustments - Removed text shadow - Slowed down background animation - Added final text + link - Changed link style

--- src/partials/banderole.php
+++ src/partials/banderole.php
@@ -1,5 +1,5 @@
 <div class="banderole">
-    <div class="banderole__text">CallOne Convention 2022</div><a href="#">Mehr erfahren</a>
+    <div class="banderole__text">Webinar 10.03.22 - 10 Booster für den Kundensupport</div><a href="https://www.linkedin.com/events/serviceexzellenz-10boosterf-rih6903034712382935040" target="_blank" rel="nofollow">Mehr erfahren</a>
 </div>
 
 <div class="banderole-spacer"></div>
