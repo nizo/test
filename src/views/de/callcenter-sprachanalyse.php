11200a576c220a71d4c8e8cabe7985d4c2820511
Max Boll <max.boll@callone.de>
2022-03-08 13:11:28 +0100

    Added missing graphics to i2x page

--- src/views/de/callcenter-sprachanalyse.php
+++ src/views/de/callcenter-sprachanalyse.php
@@ -9,25 +9,24 @@
 			</p>
 		</div>
         <div class="hero__image">
-            <!-- TODO: Add header image -->
-            <img src="/assets/images/illus/statistics-header-illustration-optimized.svg" alt="Die umfangreichen Echtzeitstatistiken der CallOne Callcenter Software haben viele Vorteile für Ihren ROI" width="465" height="430" style="width:100%" />
+            <img src="/assets/images/illus/i2x-header.png" alt="" width="" height="" style="width:100%" />
         </div>
 	</div>
 </div>
 
 <div class="section section--no-padding-top">
-<div class="section__content section__content--narrow centered mobile-left">
+    <div class="section__content section__content--narrow centered mobile-left">
         <h1>Der i2x-Agenten-Client: Das Echtzeit-Dashboard für Ihre Agenten.</h1>
         <p class="bigtext">Der i2x Client fungiert als <span class="fw500">Echtzeitcoach für die Gespräche in Sales und Service</span>. Agenten bekommen direkt <span class="fw500">in den Telefonaten</span> einen <span class="fw500">flexiblen Gesprächsleitfaden</span>.</p>
     </div>
 
     <div class="section__content section__content--wide">
         <div class="co-grid">
-            <div class="co-grid__col--6-xs co-grid__col--vcentered">
-                TODO: Add Image
+            <div class="co-grid__col co-grid__col--6-xs co-grid__col--vcentered">
+                <img src="/assets/images/illus/i2x-client.png" alt="" width="" height="" />
             </div>
-            <div class="co-grid__col--6-xs co-grid__col--vcentered">
-                <ul class="list list--checkmarks-top list--checkmarks-green bigtext">
+            <div class="co-grid__col co-grid__col--6-xs co-grid__col--vcentered">
+                <ul class="list list--checkmarks-top list--checkmarks-green" style="--fs:26px">
                     <li>Schlagwörter (so genannte Sagen & Nicht Sagen <span class="tooltip tooltip--dark"><span>Sagen sind Wörter und Phrasen, die im aktuellen Gespräch positiv bewertet sind. Nicht Sagen sind Wörter und Phrasen, die vom Agenten vermieden werden sollten.</span></span>) werden in Echtzeit übermittelt</li>
                     <li>Echtzeitanalyse zu Tonalität und Satzlänge wird ausgespielt</li>
                     <li>Auswertung der optimalen Sprechgeschwindigkeit steht zur Verfügung</li>
@@ -45,15 +44,15 @@
 
     <div class="section__content section__content--wide">
         <div class="co-grid">
-            <div class="co-grid__col--6-xs co-grid__col--vcentered">
-                <ul class="list list--checkmarks-top list--checkmarks-green bigtext">
+            <div class="co-grid__col co-grid__col--6-xs co-grid__col--vcentered">
+                <ul class="list list--checkmarks-top list--checkmarks-green" style="--fs:26px">
                     <li>Optimale Gesprächsführung durch Echtzeitcoaching im i2x Client</li>
                     <li>Flexible Gesprächsleitfäden für höhere Abschlussquoten</li>
                     <li>Hohe Transparenz und Planbarkeit Dank umfangreicher Statistiken</li>
                 </ul>
             </div>
-            <div class="co-grid__col--6-xs co-grid__col--vcentered">
-                TODO: Add Image
+            <div class="co-grid__col co-grid__col--6-xs co-grid__col--vcentered">
+                <img src="/assets/images/illus/i2x-results.png" alt="" width="" height="" />
             </div>
         </div>
     </div>
@@ -61,7 +60,7 @@
 
 <div class="section" style="background:linear-gradient(#fff 50%, #f0f0f0 50%);">
     <div class="section__content section__content--wide">
-        <div class="breather-box" style="--c-bg:#ecf9ff;--c-text:#000;">
+        <div class="breather-box" style="--c-bg:#D8E6FE;--c-text:#000;">
             <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
                 <div class="co-grid__col co-grid__col--12-xs co-grid__col--3-sm co-grid__col--vcentered">
                     <img src="/assets/images/client-logos/logo-check24-black.svg" alt="" width="255" height="61" />
@@ -78,17 +77,17 @@
     </div>
 </div>
 
-<div class="section section--grey-white-blue-half-circle-bottom">
+<div class="section section--grey-white">
     <div class="section__content section__content--wide">
         <div class="tablist">
             <div class="tablist__links tablist__links--full-width">
-                <div class="tablist__link" data-tab="1" style="--bg:linear-gradient(#ecf9ff, transparent);--stickyBg:linear-gradient(transparent, #ecf9ff);">
+                <div class="tablist__link" data-tab="1" style="--bg:linear-gradient(#3B81FB33, transparent);--stickyBg:linear-gradient(transparent, #3B81FB33);">
                     <strong>Agenten</strong>
                 </div>
-                <div class="tablist__link" data-tab="2" style="--bg:linear-gradient(#ecf9ff, transparent);--stickyBg:linear-gradient(transparent, #ecf9ff);">
+                <div class="tablist__link" data-tab="2" style="--bg:linear-gradient(#3B81FB33, transparent);--stickyBg:linear-gradient(transparent, #3B81FB33);">
                     <strong>Manager</strong>
                 </div>
-                <div class="tablist__link" data-tab="3" style="--bg:linear-gradient(#ecf9ff, transparent);--stickyBg:linear-gradient(transparent, #ecf9ff);">
+                <div class="tablist__link" data-tab="3" style="--bg:linear-gradient(#3B81FB33, transparent);--stickyBg:linear-gradient(transparent, #3B81FB33);">
                     <strong>Callcenter-Leiter</strong>
                 </div>
             </div>
@@ -96,14 +95,14 @@
             <div class="tablist__content" data-tab="1">
                 <div class="co-grid">
                     <div class="co-grid__col co-grid__col--5-xs co-grid__col--vcentered">
-                        TODO: Add image
+                        <img src="/assets/images/illus/i2x-agenten.png" alt="" width="" height="" />
                     </div>
                     <div class="co-grid__col co-grid__col--7-xs co-grid__col--vcentered">
                         <p class="bigtext">Kürzere Fallbearbeitungszeiten bei höherer Kundenzufriedenheit und Abschlussquote.</p>
 
                         <br />
 
-                        <ul class="list list--checkmarks-top bigtext">
+                        <ul class="list list--checkmarks-top list--checkmarks-green" style="--fs:26px">
                             <li>Zielgerichtete Gespräche durch Echtzeitcoaching</li>
                             <li>Flexibler Gesprächsleitfaden bringt bessere Abschlussquoten</li>
                             <li>Objekte Bewertbarkeit durch umfangreiche Analysen und Reportings</li>
@@ -115,14 +114,14 @@
             <div class="tablist__content" data-tab="2">
                 <div class="co-grid">
                     <div class="co-grid__col co-grid__col--5-xs co-grid__col--vcentered">
-                        TODO: Add image
+                        <img src="/assets/images/illus/i2x-manager.png" alt="" width="" height="" />
                     </div>
                     <div class="co-grid__col co-grid__col--7-xs co-grid__col--vcentered">
                         <p class="bigtext">Umfangreicher Überblick für datenbasierte Entscheidungen und Erhöhung Ihres ROI.</p>
 
                         <br />
 
-                        <ul class="list list--checkmarks-top bigtext">
+                        <ul class="list list--checkmarks-top list--checkmarks-green" style="--fs:26px">
                             <li>Umfangreiches Reporting für Erhöhung des ROI nutzen</li>
                             <li>Datenbasiert neue Strategien und automatisierte A/B-Tests finden</li>
                             <li>Performantere Ergebnisse der Mitarbeiter durch schnelleres Onboarding</li>
@@ -134,14 +133,14 @@
             <div class="tablist__content" data-tab="3">
                 <div class="co-grid">
                     <div class="co-grid__col co-grid__col--5-xs co-grid__col--vcentered">
-                        TODO: Add image
+                        <img src="/assets/images/illus/i2x-callcenter-leiter.png" alt="" width="" height="" />
                     </div>
                     <div class="co-grid__col co-grid__col--7-xs co-grid__col--vcentered">
                         <p class="bigtext">Die Entwicklung der Mitarbeiter effektiver beeinflussen sowie Performance steuern und objektiv bewerten.</p>
 
                         <br />
 
-                        <ul class="list list--checkmarks-top bigtext">
+                        <ul class="list list--checkmarks-top list--checkmarks-green" style="--fs:26px">
                             <li>Schnelleres Onboarding der Mitarbeiter und höhere Mitarbeiterbindung</li>
                             <li>Wichtige KPIs wie Konvertierungsrate und Kundenzufriedenheit werden objektiv bewertbar</li>
                             <li>optimale Bewertung und Steuerung der Mitarbeiter durch objektive Analysen und Reportings</li>
