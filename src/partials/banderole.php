cc4970124ca146c8bbe71391133ff6bf0396948f
Max Boll <max.boll@callone.de>
2022-03-14 10:57:45 +0100

    Banderole Update - Changed link - Added timed display option

--- src/partials/banderole.php
+++ src/partials/banderole.php
@@ -1,5 +1,14 @@
-<div class="banderole">
-    <div class="banderole__text">Booster für Ihren Kundenservice. Kostenfreies Webinar.</div><a href="https://www.linkedin.com/events/serviceexzellenz-10boosterf-rih6903034712382935040" target="_blank" rel="nofollow" id="banderole-cta" class="trackedElement" data-label="Banderole CTA" data-action="Banderole CTA Clicked" data-category="Banderole">JETZT TEILNEHMEN</a>
-</div>
+<?php
+$show_until = strtotime('2022-03-25 12:00:00');
+$banderole_link = 'https://next.edudip.com/de/webinar/insides-vom-marktfuhrer-sales-und-kundenservice-bei-check24-kreditservice/1792796';
 
-<div class="banderole-spacer"></div>
+if ($show_until > time()) {
+    ?>
+    <div class="banderole">
+        <div class="banderole__text">Booster für Ihren Kundenservice. Kostenfreies Webinar.</div><a href="<?= $banderole_link; ?>" target="_blank" rel="nofollow" id="banderole-cta" class="trackedElement" data-label="Banderole CTA" data-action="Banderole CTA Clicked" data-category="Banderole">JETZT TEILNEHMEN</a>
+    </div>
+
+    <div class="banderole-spacer"></div>
+    <?php
+}
+?>
