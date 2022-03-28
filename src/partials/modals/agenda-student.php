343863b9646d027b87ed426e8a11c7f73248b704
magdalena-callone <magdalena.lueders@callone.de>
2022-03-28 12:04:06 +0200

    added agenda for student

---
+++ src/partials/modals/agenda-student.php
@@ -0,0 +1,37 @@
+<div class="callone-modal callone-modal--no-padding" data-modal="agenda-projektmanager" data-title="Tagesagenda" data-subtitle="Projektmanager*in">
+    <div class="agenda">
+        <div class="agenda__title">Dein Zeitslot</div>
+
+        <div class="agenda__row">
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--cup">
+                    Tasse Kaffee oder Tee
+                </div>
+                <div class="agenda__item agenda__item--blue agenda__item--exchange">
+                    Operative Abstimmung mit Marketing-Team
+                </div>
+            </div>
+
+            </div>
+        </div>
+
+        <div class="agenda__row">
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--blue agenda__item--customer-support">
+                    Austausch und Ideen-Besprechung mit Team
+                </div>
+                
+            </div>
+            <div class="agenda__col">
+                <div class="agenda__item agenda__item--red agenda__item--computer">
+                    Recherche, Texten, Publishen
+                </div>
+            </div>
+        </div>
+
+        <div class="agenda__legend">
+            <div class="agenda__info agenda__info--blue">Interne Kommunikation</div>
+            <div class="agenda__info agenda__info--red">Computer-Arbeit</div>
+        </div>
+    </div>
+</div>
