dad883b4d09ca0fc873a16163145c14f1a8a780b
Max Boll <max.boll@callone.de>
2022-06-01 11:17:45 +0200

    Cleanup apodiscounter

--- src/views/de/case-study-apodiscounter.php
+++ src/views/de/case-study-apodiscounter.php
@@ -1,184 +1,181 @@
-<div class="wrapper">
-  <div class="content wide noOverflow">
-    <header class="main-header">
-      <div class="media animation">
-        <div id="lottie" class="business"></div>
-        <?php $additional_js_files[] = '/assets/animations/svg/business.js' ?>
-      </div>
-      <div class="text">
-        <?= pictureTag(
-          '/assets/images/client-logos/apo-discounter.svg',
-          'Neues Callcenter Konzept - Apodiscounter',
-          240, 48, [
-            'class' => 'logo'
-          ], false
-        ); ?>
-        <h1>Probat für Wachstum: ein neues Callcenter-Konzept</h1>
-        <p class="primary">CallOne und apo-discounter.de</p>
-      </div>
+<div class="section">
+    <header class="hero">
+        <div class="hero__text">
+            <?= pictureTag(
+                '/assets/images/client-logos/apo-discounter.svg',
+                'Neues Callcenter Konzept - Apodiscounter',
+                240, 48, [
+                    'class' => 'logo'
+                ], false
+            ); ?>
+            <h1>Probat für Wachstum: ein neues Callcenter-Konzept</h1>
+            <p class="bigtext">CallOne und apo-discounter.de</p>
+        </div>
+        <div class="hero__image">
+            <div id="lottie"></div>
+            <?php $additional_js_files[] = '/assets/animations/svg/business.js' ?>
+        </div>
     </header>
-  </div>
 </div>
 
-<div class="wrapper">
-  <div class="content narrow">
-    <div class="grid">
-      <div class="col-3">
-        <p><strong>13</strong> Online-Shops</p>
-      </div>
-      <div class="col-3">
-        <p><strong>2</strong> stationäre Apotheken</p>
-      </div>
-      <div class="col-3">
-        <p><strong>>30</strong> Service-Rufnummern</p>
-      </div>
+<div class="section section--light-green">
+    <div class="section__content secion__content--narrow">
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-sm co-grid__col--4-md">
+                <p><strong>13</strong> Online-Shops</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-sm co-grid__col--4-md">
+                <p><strong>2</strong> stationäre Apotheken</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-sm co-grid__col--4-md">
+                <p><strong>>30</strong> Service-Rufnummern</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-sm co-grid__col--4-md">
+                <p><strong>>50</strong> Skillgruppen</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-sm co-grid__col--4-md">
+                <p><strong>5</strong> Kontaktkanäle</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-sm co-grid__col--4-md">
+                <p><strong>150</strong> individuelle Ansagen</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--narrow">
+        <h2 class="centered mobile-left">Fachkundige pharmazeutische Beratung am Telefon</h2>
+        <p class="centered mobile-left bigtext">apo-discounter.de ist eine der führenden Online-Versandapotheken. Kunden können online über das Portal apo-discounter.de ihre Arzneimittel bestellen. Neben einem günstigen und umfangreichen Produktsortiment steht die telefonische Beratung im Vordergrund. Kunden nehmen diesen Service genau wie in einer Ladenapotheke in Anspruch. Zusammen mit dem Kundendienst kommt es zu einem ansehnlichen Anrufaufkommen im Callcenter. CallOne durfte dabei helfen, diese Prozesse zu optimieren.</p>
     </div>
 
-    <div class="grid">
-      <div class="col-3">
-        <p><strong>>50</strong> Skillgruppen</p>
-      </div>
-      <div class="col-3">
-        <p><strong>5</strong> Kontaktkanäle</p>
-      </div>
-      <div class="col-3">
-        <p><strong>150</strong> individuelle Ansagen</p>
-      </div>
+    <div class="section__content section__content--wide">
+        <div class="content-box content-box--shadow content-box--full-width">
+            <div class="quote" style="--fontSize:30px">
+                „Wir setzen in allen Bereichen auf Schnelligkeit, Kompetenz und modernste Technik. So können wir unsere Kunden von unserem Service begeistern und genauso hat uns auch CallOne begeistert!“
+            </div>
+            <p>
+                <strong>Robert Kudrass</strong><br />
+                Bereichsmanager Betrieb &amp; Vertrieb
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--narrow">
+        <h2 class="centered mobile-left">Herausforderungen & Ziele</h2>
+        <p class="centered mobile-left bigtext">Einer der wichtigsten Eckpfeiler der Zusammenarbeit war die strikte Trennung von Kundendienst und pharmazeutischer Beratung. In diesem Zuge konnten wir weitere Bereiche optimieren.</p>
+
+        <div class="content-box content-box--full-width content-box--shadow">
+            <ul class="list list--checkmarks list--checkmarks-green">
+                <li>Bessere Anrufverteilung</li>
+                <li>Wartezeiten verkürzen</li>
+                <li>Erreichbarkeit verbessern</li>
+                <li>Trennung von Kundendienst und Beratung</li>
+                <li>Integration in bestehende Telefonanlage</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--wide">
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
+                <p class="centered">
+                    <?= pictureTag(
+                        '/assets/images/icons_svg/small_18.svg',
+                        'Multichannel',
+                        67, 64, [
+                            'style' => 'height:64px'
+                        ]
+                    ); ?>
+                </p>
+                <h3 class="centered">Warteschleifen-<br />management</h3>
+                <ul class="list list--checkmarks list--checkmarks-top">
+                    <li>Last-Agent-Routing</li>
+                    <li>Wartezeitansage</li>
+                    <li>Anruferpriorisierung</li>
+                    <li>Rückruffunktion aus der Warteschleife</li>
+                </ul>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
+                <p class="centered">
+                    <?= pictureTag(
+                        '/assets/images/icons_svg/small_07_2.svg',
+                        'administrationslos',
+                        87, 64, [
+                            'style' => 'height:64px'
+                        ]
+                    ); ?>
+                </p>
+                <h3 class="centered">Beste<br />Anrufsteuerung</h3>
+                <ul class="list list--checkmarks list--checkmarks-top">
+                    <li>0800 VIP-Rufnummern</li>
+                    <li>Integration diverser Auswahlmenüs</li>
+                    <li>Partnereinbindung</li>
+                    <li>Intelligente Überlauffunktion</li>
+                    <li>Ursprungsrouting</li>
+                </ul>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
+                <p class="centered">
+                    <?= pictureTag(
+                        '/assets/images/icons_svg/small_19.svg',
+                        'Erreichbarkeit',
+                        64, 64, [
+                            'style' => 'height:64px'
+                        ]
+                    ); ?>
+                </p>
+                <h3 class="centered">Mehr<br />Features</h3>
+                <ul class="list list--checkmarks list--checkmarks-top">
+                    <li>Optionale Gesprächsaufzeichnung</li>
+                    <li>Sonderstatistiken</li>
+                    <li>Callback auf Kundenwebseite</li>
+                    <li>SIP-Trunking via PSTN und IP</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section section--light-green">
+  <div class="section__content section__content--narrow">
+        <h2 class="centered mobile-left">Ergebnisse</h2>
+
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--vcentered">
+                <p><strong>90 %</strong>  Verringerung der Wartezeit</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--vcentered">
+                <p><strong>80 %</strong> Steigerung der Kundenzufriedenheit</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--vcentered">
+                <p><strong>95 %</strong> Erhöhung der durchschnittlichen Erreichbarkeit</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--narrow">
+        <h2 class="centered mobile-left">Guter Service</h2>
+        <p class="centered mobile-left bigtext">Apo-discounter.de hat schon immer sehr viel Wert auf guten Kundenservice gelegt und hat nun die nötigen Werkzeuge, um diesen zu perfektionieren. Auf Grundlage der gelungenen Zusammenarbeit von apo-discounter.de und CallOne steht schon ein baldiger Ausbau der Lösung auf dem Programm.</p>
     </div>
 
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content narrow">
-    <h2>Fachkundige pharmazeutische Beratung am Telefon</h2>
-    <p>apo-discounter.de ist eine der führenden Online-Versandapotheken. Kunden können online über das Portal apo-discounter.de ihre Arzneimittel bestellen. Neben einem günstigen und umfangreichen Produktsortiment steht die telefonische Beratung im Vordergrund. Kunden nehmen diesen Service genau wie in einer Ladenapotheke in Anspruch. Zusammen mit dem Kundendienst kommt es zu einem ansehnlichen Anrufaufkommen im Callcenter. CallOne durfte dabei helfen, diese Prozesse zu optimieren.</p>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content wide">
-    <div class="shadow-box">
-      <blockquote>
-        <p>„Wir setzen in allen Bereichen auf Schnelligkeit, Kompetenz und modernste Technik. So können wir unsere Kunden von unserem Service begeistern und genauso hat uns auch CallOne begeistert!“ </p>
-        <p class="bold">
-          Robert Kudrass<br />
-          Bereichsmanager Betrieb &amp; Vertrieb
-        </p>
-      </blockquote>
+    <div class="section__content section__content--wide">
+        <div class="content-box content-box--full-width content-box--shadow">
+            <div class="quote" style="--fontSize:30px">
+                „Mit der neuen Callcenter-Lösung konnten wir unsere Erreichbarkeit um ein Vielfaches steigern. Durch unsere Expansion stieg unser Callvolumen sehr stark an. Dank CallOne können wir diese Anrufe meist ohne Wartezeiten annehmen. Und sollte es doch einmal zu Wartezeiten kommen, geht uns der Kunde nicht verloren sondern wird transparent informiert.“
+            </div>
+            <p>
+                <strong>Wytze Kempenaar</strong><br />
+                Projektmanager bei apo-discounter.de
+            </p>
+        </div>
     </div>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content narrow">
-    <h2>Herausforderungen & Ziele</h2>
-    <p>Einer der wichtigsten Eckpfeiler der Zusammenarbeit war die strikte Trennung von Kundendienst und pharmazeutischer Beratung. In diesem Zuge konnten wir weitere Bereiche optimieren.</p>
-
-    <div class="shadow-box padding">
-      <ol>
-        <li>Bessere Anrufverteilung</li>
-        <li>Wartezeiten verkürzen</li>
-        <li>Erreichbarkeit verbessern</li>
-        <li>Trennung von Kundendienst und Beratung</li>
-        <li>Integration in bestehende Telefonanlage</li>
-      </ol>
-    </div>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content wide">
-    <div class="grid">
-      <div class="col-3">
-        <?= pictureTag(
-          '/assets/images/icons_svg/small_18.svg',
-          'Multichannel',
-          70, 67, [
-            'class' => 'icon'
-          ]
-        ); ?>
-        <h3>Warteschleifenmanagement</h3>
-          <ul>
-            <li>Last-Agent-Routing</li>
-            <li>Wartezeitansage</li>
-            <li>Anruferpriorisierung</li>
-            <li>Rückruffunktion aus der Warteschleife</li>
-          </ul>
-      </div>
-      <div class="col-3">
-        <?= pictureTag(
-          '/assets/images/icons_svg/small_07_2.svg',
-          'administrationslos',
-          70, 52, [
-            'class' => 'icon'
-          ]
-        ); ?>
-        <h3>Anrufsteuerung</h3>
-        <ul>
-          <li>0800 VIP-Rufnummern</li>
-          <li>Integration diverser Auswahlmenüs</li>
-          <li>Partnereinbindung</li>
-          <li>Intelligente Überlauffunktion</li>
-          <li>Ursprungsrouting</li>
-        </ul>
-      </div>
-      <div class="col-3">
-        <?= pictureTag(
-          '/assets/images/icons_svg/small_19.svg',
-          'Erreichbarkeit',
-          70, 70, [
-            'class' => 'icon'
-          ]
-        ); ?>
-        <h3>Mehr Features</h3>
-        <ul>
-          <li>Optionale Gesprächsaufzeichnung</li>
-          <li>Sonderstatistiken</li>
-          <li>Callback auf Kundenwebseite</li>
-          <li>SIP-Trunking via PSTN und IP</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content narrow">
-    <h2>Ergebnisse</h2>
-    <div class="grid">
-      <div class="col-3">
-        <p><strong>90 %</strong>  Verringerung der Wartezeit</p>
-      </div>
-      <div class="col-3">
-        <p><strong>80 %</strong> Steigerung der Kundenzufriedenheit</p>
-      </div>
-      <div class="col-3">
-        <p><strong>95 %</strong> Erhöhung der durchschnittlichen Erreichbarkeit</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content narrow">
-    <h2>Guter Service</h2>
-    <p>Apo-discounter.de hat schon immer sehr viel Wert auf guten Kundenservice gelegt und hat nun die nötigen Werkzeuge, um diesen zu perfektionieren. Auf Grundlage der gelungenen Zusammenarbeit von apo-discounter.de und CallOne steht schon ein baldiger Ausbau der Lösung auf dem Programm.</p>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content wide">
-    <div class="shadow-box">
-      <blockquote>
-        <p> „Mit der neuen Callcenter-Lösung konnten wir unsere Erreichbarkeit um ein Vielfaches steigern. Durch unsere Expansion stieg unser Callvolumen sehr stark an. Dank CallOne können wir diese Anrufe meist ohne Wartezeiten annehmen. Und sollte es doch einmal zu Wartezeiten kommen, geht uns der Kunde nicht verloren sondern wird transparent informiert.“ </p>
-        <p class="bold">
-          Wytze Kempenaar<br />
-          Projektmanager bei apo-discounter.de
-        </p>
-      </blockquote>
-    </div>
-  </div>
 </div>
 
 <?php include('partials/slider.php'); ?>
