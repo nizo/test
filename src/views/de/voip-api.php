997e24ba02b84f94163e1600457b7aef6258ba78
Max Boll <max.boll@callone.de>
2022-06-02 11:03:43 +0200

    Cleanup voip-api

--- src/views/de/voip-api.php
+++ src/views/de/voip-api.php
@@ -1,10 +1,10 @@
 <div class="section section--light-grey">
     <div class="hero">
-        <div class="hero__texttext">
+        <div class="hero__text">
             <h1>Volle Flexibilität mit Telefon-Schnittstellen</h1>
             <p class="bigtext">Kommunikation muss für Sie ohne Stress funktionieren. Passen Sie CallOne einfach mit unseren offenen Schnittstellen nach Ihren Anforderungen an.</p>
             <p class="mobile-centered">
-                <a class="button tertiary rounded" href="https://connect.callone.io/api/rest/" target="_blank">Unsere Dokumentation <span class="button__icon arrow icon-jump-forward"></span></a>
+                <a class="btn btn--primary btn--arrow-right" href="https://connect.callone.io/api/rest/" target="_blank">Unsere Dokumentation</a>
             </p>
         </div>
         <div class="hero__image">
@@ -17,127 +17,168 @@
     </div>
 </div>
 
-<div class="wrapper no-margin-top white-lightgreen">
-  <div class="content wide padding-top-80">
-    <div class="grid">
-      <div class="col-3-5">
-        <h2>Vorteile der CallOne APIs</h2>
+<div class="section section--light-green">
+    <div class="section__content section__content--wide">
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--8-md co-grid__col--vcentered">
+                <h2>Vorteile der CallOne APIs</h2>
 
-        <p>Egal, ob Sie Calls direkt über Ihr CRM annehmen, auslösen, verwalten oder Ihre Telefonanlage direkt mit Ihren Business-Tools verbinden wollen &ndash; mit unseren fertigen Schnittstellen optimieren Sie Ihre Prozesse.</p>
+                <p>Egal, ob Sie Calls direkt über Ihr CRM annehmen, auslösen, verwalten oder Ihre Telefonanlage direkt mit Ihren Business-Tools verbinden wollen &ndash; mit unseren fertigen Schnittstellen optimieren Sie Ihre Prozesse.</p>
 
-        <div class="grid">
-          <div class="col-2 big-float-number">
-            <div class="number">1</div>
-            <div class="text">Zugängliche, gut dokumentierte Bibliotheken mit modularer Architektur</div>
-          </div>
-          <div class="col-2 big-float-number">
-            <div class="number">2</div>
-            <div class="text">Spezifisch gebaut von unseren eigenen Entwicklern &ndash; sauber und funktional</div>
-          </div>
-          <div class="col-2 big-float-number">
-            <div class="number">3</div>
-            <div class="text">Fast alle Funktionen sind browserkompatibel und damit überall einsetzbar</div>
-          </div>
-          <div class="col-2 big-float-number">
-            <div class="number">4</div>
-            <div class="text">Stetige Pflege, Erweiterung und Optimierung unserer Bobliotheken</div>
-          </div>
-        </div>
-      </div>
-      <div class="col-2-5">
-        <h2>Unsere Schnittstellen</h2>
-
-        <p>
-          <a class="button secondary black mobileWrap small-rounded" href="https://connect.callone.io/api/rest/" target="_blank"><span class="button__icon icon-documentation"></span> Zur API-Dokumentation</a>
-        </p>
-
-        <div class="scroller fadeOut-lightgrey-2" data-height="300">
-          <div class="scroller-belt">
-            <div class="scroller-content">
-              <h2>Click-to-Call API</h2>
-              <p>Anrufe über API auslösen &ndash; egal ob stationäres Telefon, Softphone-Client oder direkt über den Browser</p>
-              
-              <h2>DTMF API</h2>
-              <p>Empfangen Sie Tasteneingaben (DTMF-Töne) und übermitteln Sie diese direkt an Ihre Businesstools</p>
-
-              <h2>ACD API</h2>
-              <p>Administrieren Sie neue KundenserviceagentInnen Ihrer Callcenter-Lösung über die API direkt in Ihrem CRM</p>
-
-              <h2>Live API</h2>
-              <p>Alle aktuellen Anrufe und Wartezeiten von ACD und IP-Telefonanlagen per API in Echtzeit abrufbar</p>
-
-              <h2>Statistics API</h2>
-              <p>Alle Statistiken in Echtzeit per API abrufbar und in andere Systeme integrierbar</p>
-
-              <h2>Text2Speech API</h2>
-              <p>Spielen Sie z.B. auf Basis des Versandstatus des Kunden eine passende, individuelle Ansage ein</p>
-
-              <h2>Call Tracking API</h2>
-              <p>Anrufinformationen aus ACD und VoIP-Telefonanlagen in Echtzeit erhalten</p>
-
-              <h2>Listen API</h2>
-              <p>In Sekundenschnelle neue Anruferlisten anlegen oder Listeneinträge abfragen</p>
-
-              <h2>SMS API</h2>
-              <p>SMS jederzeit in Echtzeit an Kunden und Interessenten versenden</p>
-
-              <h2>SIP API</h2>
-              <p>Legen Sie neue VoIP-Nebenstellen direkt aus Ihrem CRM oder Personaltool an</p>
-
-              <h2>Fax API</h2>
-              <p>Faxe mit unterschiedlichen Absenderkennungen flexibel direkt per API verschicken</p>
-
-              <h2>Dateien API</h2>
-              <p>Gesprächsaufzeichnungen in Ihren eigenen Systemen ablegen und analysieren, sowie Wartemusik und Ansagen austauschen</p>
+                <div class="co-grid">
+                    <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-md">
+                        <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                            <div class="co-grid__col" style="font-size:72px">
+                                1
+                            </div>
+                            <div class="co-grid__col co-grid__col--fill co-grid__col--vcentered">
+                                Zugängliche, gut dokumentierte Bibliotheken mit modularer Architektur
+                            </div>
+                        </div>
+                    </div>
+                    <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-md">
+                        <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                            <div class="co-grid__col" style="font-size:72px">
+                                2
+                            </div>
+                            <div class="co-grid__col co-grid__col--fill co-grid__col--vcentered">
+                                Spezifisch gebaut von unseren eigenen Entwicklern &ndash; sauber und funktional
+                            </div>
+                        </div>
+                    </div>
+                    <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-md">
+                        <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                            <div class="co-grid__col" style="font-size:72px">
+                                3
+                            </div>
+                            <div class="co-grid__col co-grid__col--fill co-grid__col--vcentered">
+                                Fast alle Funktionen sind browserkompatibel und damit überall einsetzbar
+                            </div>
+                        </div>
+                    </div>
+                    <div class="co-grid__col co-grid__col--12-xs co-grid__col--6-md">
+                        <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                            <div class="co-grid__col" style="font-size:72px">
+                                4
+                            </div>
+                            <div class="co-grid__col co-grid__col--fill co-grid__col--vcentered">
+                                Stetige Pflege, Erweiterung und Optimierung unserer Bobliotheken
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--vcentered">
+                <h2>Unsere Schnittstellen</h2>
+
+                <p>
+                    <a class="btn btn--secondary" href="https://connect.callone.io/api/rest/" target="_blank">Zur API-Dokumentation</a>
+                </p>
+
+                <div class="content-box content-box--white content-box--shadow">
+                    <div class="scroller scroller--fade-white" data-height="300">
+                        <div class="scroller-belt">
+                            <div class="scroller-content">
+                                <h2>Click-to-Call API</h2>
+                                <p>Anrufe über API auslösen &ndash; egal ob stationäres Telefon, Softphone-Client oder direkt über den Browser</p>
+                                
+                                <h2>DTMF API</h2>
+                                <p>Empfangen Sie Tasteneingaben (DTMF-Töne) und übermitteln Sie diese direkt an Ihre Businesstools</p>
+
+                                <h2>ACD API</h2>
+                                <p>Administrieren Sie neue KundenserviceagentInnen Ihrer Callcenter-Lösung über die API direkt in Ihrem CRM</p>
+
+                                <h2>Live API</h2>
+                                <p>Alle aktuellen Anrufe und Wartezeiten von ACD und IP-Telefonanlagen per API in Echtzeit abrufbar</p>
+
+                                <h2>Statistics API</h2>
+                                <p>Alle Statistiken in Echtzeit per API abrufbar und in andere Systeme integrierbar</p>
+
+                                <h2>Text2Speech API</h2>
+                                <p>Spielen Sie z.B. auf Basis des Versandstatus des Kunden eine passende, individuelle Ansage ein</p>
+
+                                <h2>Call Tracking API</h2>
+                                <p>Anrufinformationen aus ACD und VoIP-Telefonanlagen in Echtzeit erhalten</p>
+
+                                <h2>Listen API</h2>
+                                <p>In Sekundenschnelle neue Anruferlisten anlegen oder Listeneinträge abfragen</p>
+
+                                <h2>SMS API</h2>
+                                <p>SMS jederzeit in Echtzeit an Kunden und Interessenten versenden</p>
+
+                                <h2>SIP API</h2>
+                                <p>Legen Sie neue VoIP-Nebenstellen direkt aus Ihrem CRM oder Personaltool an</p>
+
+                                <h2>Fax API</h2>
+                                <p>Faxe mit unterschiedlichen Absenderkennungen flexibel direkt per API verschicken</p>
+
+                                <h2>Dateien API</h2>
+                                <p>Gesprächsaufzeichnungen in Ihren eigenen Systemen ablegen und analysieren, sowie Wartemusik und Ansagen austauschen</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 
-<div class="wrapper no-margin-top dark-light-grey">
-  <div class="content wide">
-    <h2 class="centered">
-      Schnelle und professionelle<br />
-      Implementierung mit <span class="color-green font-headline">100% Datenschutz</span>
-    </h2>
+<div class="section section--dark-green-black">
+    <div class="section__content section__content--wide">
+        <h2 class="centered mobile-left">
+            Schnelle und professionelle<br />
+            Implementierung mit <span class="color-green">100% Datenschutz</span>
+        </h2>
 
-    <div class="grid">
-      <div class="col-3 card">
-        <div class="card__image">
-          <?= pictureTag(
-            '/assets/images/icons_svg/usp-speed-icon.svg',
-            'Call Center Software wird schnell bereitgestellt',
-            148, 46
-          ); ?>
+        <div class="co-grid">
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md card">
+                <div class="content-box content-box--white content-box--small-padding">
+                    <p class="centered">
+                        <?= pictureTag(
+                            '/assets/images/icons_svg/usp-speed-icon.svg',
+                            'Call Center Software wird schnell bereitgestellt',
+                            161, 50, [
+                                'style' => 'height: 50px;'
+                            ]
+                        ); ?>
+                    </p>
+                    <br />
+                    <h3>Bereitstellung in wenigen Tagen statt Monaten</h3>
+                    <p>Ärgern Sie sich nicht mit Self-Service herum. An Ihre Organisation angepasste Telefonie-Lösungen kann CallOne dank erfahrener Projektmanager &amp; Schnittstellenentwickler in kürzester Zeit live bringen.</p>
+                </div>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md card">
+                <div class="content-box content-box--white content-box--small-padding">
+                    <p class="centered">
+                        <?= pictureTag(
+                            '/assets/images/icons_svg/usp-germany-icon.svg',
+                            'Call Center Software mit hohem deutschen Datenschutzstandard',
+                            44, 50, [
+                                'style' => 'height: 50px;'
+                            ]
+                        ); ?>
+                    </p>
+                    <br />
+                    <h3>Telefonie mit deutschem DSGVO-Standard</h3>
+                    <p>Das Privacy Shield Abkommen zwischen USA und EU ist nun gekippt. Zum Glück sind Sie mit CallOne immer auf der sicheren Seite: Wir hosten unsere Telefonie-Lösung ausschließlich in Deutschland.</p>
+                </div>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md card">
+                <div class="content-box content-box--white content-box--small-padding">
+                    <p class="centered">
+                        <?= pictureTag(
+                            '/assets/images/icons_svg/usp-availability-icon.svg',
+                            'Call Center Software ist sehr gut erreichbar',
+                            92, 50, [
+                                'style' => 'height: 50px;'
+                            ]
+                        ); ?>
+                    </p>
+                    <br />
+                    <h3>Garantierte <br />99,999% <br />Erreichbarkeit</h3>
+                    <p>Dank Backup-Server, umfangreichem DDoS-Schutz und individueller Redundanzkonzepte laufen CallOne Lösungen stabil und sind rund um die Uhr für Sie und Ihre Kunden erreichbar.</p>
+                </div>
+            </div>
         </div>
-        <h3>Bereitstellung in wenigen Tagen statt Monaten</h3>
-        <p>Ärgern Sie sich nicht mit Self-Service herum. An Ihre Organisation angepasste Telefonie-Lösungen kann CallOne dank erfahrener Projektmanager &amp; Schnittstellenentwickler in kürzester Zeit live bringen.</p>
-      </div>
-      <div class="col-3 card">
-        <div class="card__image">
-          <?= pictureTag(
-            '/assets/images/icons_svg/usp-germany-icon.svg',
-            'Call Center Software mit hohem deutschen Datenschutzstandard',
-            86, 98
-          ); ?>
-        </div>
-        <h3>Telefonie mit deutschem Datenschutz-Standard</h3>
-        <p>Das Privacy Shield Abkommen zwischen USA und EU ist nun gekippt. Zum Glück sind Sie mit CallOne immer auf der sicheren Seite: Wir hosten unsere Telefonie-Lösung ausschließlich in Deutschland.</p>
-      </div>
-      <div class="col-3 card">
-        <div class="card__image">
-          <?= pictureTag(
-            '/assets/images/icons_svg/usp-availability-icon.svg',
-            'Call Center Software ist sehr gut erreichbar',
-            110, 60
-          ); ?>
-        </div>
-        <h3>Garantierte 99,999% Erreichbarkeit</h3>
-        <p>Dank Backup-Server, umfangreichem DDoS-Schutz und individueller Redundanzkonzepte laufen CallOne Lösungen stabil und sind rund um die Uhr für Sie und Ihre Kunden erreichbar.</p>
-      </div>
     </div>
-  </div>
 </div>
