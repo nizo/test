bfbbeb3bb668f3347f9fdf6dde6d44b5287b235a
Max Boll <max.boll@callone.de>
2022-06-02 15:34:09 +0200

    Cleanup 404

--- src/views/de/404.php
+++ src/views/de/404.php
@@ -1,32 +1,26 @@
-<div class="wrapper no-overflow">
-  <div class="content wide">
-  </div>
-  <div class="content wide">
-    <div class="shadow-box padding animation-1 lazyBackground">
-      <div class="grid">
-      	<div class="col-2">
-      		<h1 class="error404">404</h1>
-    	</div>
-      	<div class="col-2">
-      		<h2>Diese Seite konnte nicht gefunden werden!</h2>
-      		<p>
-      			Gründe dafür könnten sein, dass Sie eine falsche oder veraltete URL aufgerufen haben &ndash; bitte überprüfen Sie diese noch einmal.<br>
-      			Oder aber wir haben die betreffende Seite archiviert, verschoben oder umbenannt.
-      		</p>
-      		<p>
-      			Vielleicht können Sie den von Ihnen gewünschten Inhalt über unsere Seite finden.
-      		</p>
-      		<div class="button-set centered">
-      			<a href="/" class="button primary">Zur Startseite</a>
-      		</div>
-      		<p></p>
-      		<p>
-      			Oder dürfen wir Ihnen direkt behilflich sein? Schreiben Sie uns eine <a href="mailto:kontakt@callone.de">E-Mail</a> oder rufen Sie uns einfach unter <a href="tel:+493092033500">030 920 33 500</a> an.
-      		</p>
-      	</div>
-      </div>
-      
-    </div>
-  </div>
+<div class="section section--light-green-white">
+	<div class="section__content section__content--narrow">
+		<h1 class="centered mobile-left">
+			Diese Seite konnte <br class="mobile-hidden" />nicht gefunden werden!
+		</h1>
+
+		<div class="content-box content-box--white content-box--full-width content-box--shadow animation-1 lazyBackground">
+			<h2 class="centered mobile-left">Fehler 404</h2>
+
+			<p>
+				Gründe dafür könnten sein, dass Sie eine falsche oder veraltete URL aufgerufen haben &ndash; bitte überprüfen Sie diese noch einmal.<br>
+				Oder aber wir haben die betreffende Seite archiviert, verschoben oder umbenannt.
+			</p>
+			<p>
+				Vielleicht können Sie den von Ihnen gewünschten Inhalt über unsere Seite finden.
+			</p>
+			<p class="btn-set btn-set--centered">
+				<a href="/" class="btn btn--primary">Zur Startseite</a>
+			</p>
+			<p>
+				Oder dürfen wir Ihnen direkt behilflich sein? Schreiben Sie uns eine <a href="mailto:kontakt@callone.de" class="inline-link">E-Mail</a> oder rufen Sie uns einfach unter <a href="tel:+493092033500" class="inline-link">030 920 33 500</a> an.
+			</p>
+		</div>
+	</div>
 </div>
 
