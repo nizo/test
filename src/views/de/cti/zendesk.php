fc95efa583e2a0993df978701cca5f8dcef17712
Max Boll <max.boll@callone.de>
2022-06-02 11:37:42 +0200

    Cleanup zendesk

--- src/views/de/cti/zendesk.php
+++ src/views/de/cti/zendesk.php
@@ -1,109 +1,114 @@
 <div class="section">
-  <div class="section__content section__content--wide">
-    <header class="main-header">
-      <div class="text no-padding">
-        <h1>CallOne Zendesk Integration</h1>
-        <p class="primary">Bester Kundenservice und effiziente Agenten — integrieren Sie Voice Tickets in Zendesk für ein optimales Kundenerlebnis auf allen Kanälen.</p>
-      </div>
-      <div class="media centered">
-        <?= pictureTag(
-          '/assets/images/logo/partnerlogos/logo-zendesk2.svg',
-          'Zendesk',
-          293, 204, [
-            'class' => 'logo'
-          ], false
-        ); ?>
-      </div>
+    <header class="hero">
+        <div class="hero__text">
+            <h1>CallOne Zendesk Integration</h1>
+            <p class="bigtext">Bester Kundenservice und effiziente Agenten — integrieren Sie Voice Tickets in Zendesk für ein optimales Kundenerlebnis auf allen Kanälen.</p>
+        </div>
+        <div class="hero__image">
+            <?= pictureTag(
+                '/assets/images/logo/partnerlogos/logo-zendesk2.svg',
+                'Zendesk',
+                293, 204, [], false
+            ); ?>
+        </div>
     </header>
-  </div>
 </div>
 
-<div class="wrapper">
-  <div class="content wide centered">
-    <h2 class="h1">Voice Tickets</h2>
-    <p>Integrieren Sie Voice Tickets in Zendesk für effizienteren Kundenservice.</p>
-  </div>
-</div>
-
-<div class="wrapper">
-  <div class="content wide centered">
-    <div class="grid">   
-      <div class="col-3">
-        <span class="sl sl-chat-acknowledge sl-size-xl"></span>
-        <h3>Voice Tickets für eingehende Anrufe</h3>
-		<p>Mit der Zendesk-Integration erhalten Sie für jeden eingehenden Anruf automatisch ein Voice Ticket in Zendesk.</p>
-      </div>
-      <div class="col-3">
-        <span class="sl sl-king sl-size-xl"></span>
-        <h3>Tickets direkt dem Kunden zuordnen</h3>
-		<p>Voice Tickets werden anhand der Rufnummer automatisch bestehenden Kunden zugeordnet, können manuell von Ihnen zugeordnet werden oder werden als neues Ticket erstellt.</p>
-      </div>
-      <div class="col-3">
-        <span class="sl sl-ripped-note sl-size-xl"></span>
-       	<h3>Outbound Anrufe als Tickets</h3>
-		<p>Auch Ihre ausgehenden Anrufe können Sie als Ticket in Zendeskintegrieren und bekommen dazu alle Informationen zum Anruf mitgegeben.</p>
-      </div>
+<div class="section">
+    <div class="section__content section__content--narrow centered mobile-left">
+        <h2>Voice Tickets</h2>
+        <p class="bigtext">Integrieren Sie Voice Tickets in Zendesk für effizienteren Kundenservice.</p>
     </div>
-  </div>
+
+    <div class="section__content section__content--wide centered mobile-left">
+        <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
+                <span class="sl sl-chat-acknowledge sl-size-xl"></span>
+                <h3>Voice Tickets für eingehende Anrufe</h3>
+                <p>Mit der Zendesk-Integration erhalten Sie für jeden eingehenden Anruf automatisch ein Voice Ticket in Zendesk.</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
+                <span class="sl sl-king sl-size-xl"></span>
+                <h3>Tickets direkt dem Kunden zuordnen</h3>
+                <p>Voice Tickets werden anhand der Rufnummer automatisch bestehenden Kunden zugeordnet, können manuell von Ihnen zugeordnet werden oder werden als neues Ticket erstellt.</p>
+            </div>
+            <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
+                <span class="sl sl-ripped-note sl-size-xl"></span>
+                <h3>Outbound Anrufe als Tickets</h3>
+                <p>Auch Ihre ausgehenden Anrufe können Sie als Ticket in Zendeskintegrieren und bekommen dazu alle Informationen zum Anruf mitgegeben.</p>
+            </div>
+        </div>
+    </div>
 </div>
 
-<div class="wrapper" id="anbindungen">
-  <div class="content wide">
-  <h2 class="h1 centered">ACD</h2>
-  <h3 class="centered">Optimierte Kundenprozesse mit der CallOne Cloud ACD und Zendesk</h3>
-    <div class="shadow-box padding">
-      <div class="image-grid">
-        <div class="image centered">
-          <?= pictureTag(
-            '/assets/images/photos/zendesk.png',
-            'CallOne Cloud ACD und Zendesk',
-            260, 248
-          ); ?>
+<div class="section" id="anbindungen">
+    <div class="section__content section__content--wide">
+        <h2 class="centered mobile-left">
+            ACD:
+            Optimierte Kundenprozesse mit der CallOne Cloud ACD und Zendesk
+        </h2>
+
+        <div class="content-box content-box--full-width content-box--shadow">
+            <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--vcentered">
+                    <?= pictureTag(
+                        '/assets/images/photos/zendesk.png',
+                        'CallOne Cloud ACD und Zendesk',
+                        260, 248, [
+                            'class' => 'co-grid__image--full-width'
+                        ]
+                    ); ?>
+                </div>
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--8-md co-grid__col--vcentered">
+                    <h3>Zuordnung von Tickets zu Agenten</h3>
+                    <p>Durch Hinterlegen der Zendesk User-ID in Ihrer CallOne Cloud ACD, können Anrufe der Agenten völlig automatisiert dem richtigen Zendesk-User zugeordnet werden.</p>
+                    <h3>Implementierung über ID und Token</h3>
+                    <p>Um Ihnen den ZendeskKonnektor freizuschalten, benötigen wir lediglich Ihre Zendesk-ID, einen Admin-User sowie ein generiertes Token.</p>
+                    <h3>Überblick über Callcenter-Daten</h3>
+                    <p>Mit Hilfe des ZendeskKonnektor finden Sie in Zendeskalle Informationen zu angewählten Agenten, Skillgruppen, Hotlines und Warteschleife.</p>
+                </div>
+            </div>    
         </div>
-        <div class="text">
-            <h3>Zuordnung von Tickets zu Agenten</h3>
-            <p>Durch Hinterlegen der Zendesk User-ID in Ihrer CallOne Cloud ACD, können Anrufe der Agenten völlig automatisiert dem richtigen Zendesk-User zugeordnet werden.</p>
-    		<h3>Implementierung über ID und Token</h3>
-    		<p>Um Ihnen den ZendeskKonnektor freizuschalten, benötigen wir lediglich Ihre Zendesk-ID, einen Admin-User sowie ein generiertes Token.</p>
-    		<h3>Überblick über Callcenter-Daten</h3>
-    		<p>Mit Hilfe des ZendeskKonnektor finden Sie in Zendeskalle Informationen zu angewählten Agenten, Skillgruppen, Hotlines und Warteschleife.</p>
-        </div>
-      </div>    
     </div>
-  </div>
 </div>
 
-<div class="wrapper" id="statistiken">
-  <div class="content wide">
-  <h2 class="h1 centered">Statistiken</h2>
-  <h3 class="centered">Controlling nur noch an einer Stelle</h3>
-    <div class="shadow-box padding">
-      <div class="image-grid">
-        <div class="text">
-            <h3>Gruppenfunktionen</h3>
-            <p>Unterteilen Sie z.B. Ihre Hotline-Rufnummern in verschiedene Gruppen, um Anrufe bestmöglich in Zendeskauswerten zu können.</p>
-            <h3>Übergabe individueller Parameter</h3>
-            <p>Auf Wunsch übergeben wir Ihnen über die Schnittstelle auch individuelle Parameter wie Eingaben in IVR-Menüs, die Sie z.B. in Zendesk-Feldern oder Tickets anzeigen lassen können.</p>
-            <h3>Integration von Statistiken</h3>
-            <p>Integrieren Sie alle ACD- und Rufnummernstatistiken ganz einfach in Zendesk, um den Überblick über alle telefonischen Kontakte mit Ihren Kunden zu haben.</p>
+<div class="section" id="statistiken">
+    <div class="section__content section__content--wide">
+        <h2 class="centered mobile-left">
+            Statistiken:<br />
+            Controlling nur noch an einer Stelle
+        </h2>
+
+        <div class="content-box content-box--full-width content-box--shadow">
+            <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--8-md co-grid__col--order-2-xs co-grid__col--vcentered">
+                    <h3>Gruppenfunktionen</h3>
+                    <p>Unterteilen Sie z.B. Ihre Hotline-Rufnummern in verschiedene Gruppen, um Anrufe bestmöglich in Zendeskauswerten zu können.</p>
+                    <h3>Übergabe individueller Parameter</h3>
+                    <p>Auf Wunsch übergeben wir Ihnen über die Schnittstelle auch individuelle Parameter wie Eingaben in IVR-Menüs, die Sie z.B. in Zendesk-Feldern oder Tickets anzeigen lassen können.</p>
+                    <h3>Integration von Statistiken</h3>
+                    <p>Integrieren Sie alle ACD- und Rufnummernstatistiken ganz einfach in Zendesk, um den Überblick über alle telefonischen Kontakte mit Ihren Kunden zu haben.</p>
+                </div>
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--order-1-xs co-grid__col--vcentered">
+                    <?= pictureTag(
+                        '/assets/images/photos/zendesk-acd-stats.png',
+                        'Zendesk ACD Statistiken',
+                        260, 244, [
+                            'class' => 'co-grid__image--full-width'
+                        ]
+                    ); ?>
+                </div>
+            </div>    
         </div>
-        <div class="image centered">
-          <?= pictureTag(
-            '/assets/images/photos/zendesk-acd-stats.png',
-            'Zendesk ACD Statistiken',
-            260, 244
-          ); ?>
-        </div>
-      </div>    
     </div>
-  </div>
 </div>
 
 <div class="section section--black">
-  <div class="section__content section__content--wide centered">
-  	<h2>Probieren Sie es einfach, kostenlos und unverbindlich aus</h2>
-    <div class="button-set centered">
-      <a href="#" class="btn btn--primary" data-openmodal="contact-sales">Jetzt testen</a>
-    </div>    
-  </div>
+    <div class="section__content section__content--wide centered">
+        <h2 class="centered mobile-left">Probieren Sie es einfach, kostenlos und unverbindlich aus</h2>
+
+        <div class="btn-set btn-set--centered">
+            <a href="#" class="btn btn--primary" data-openmodal="contact-sales">Jetzt testen</a>
+        </div>    
+    </div>
 </div>
