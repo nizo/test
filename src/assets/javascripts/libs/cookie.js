32006d698b37677edd7b66b6232f3af7670bed3b
Max Boll <max.boll@callone.de>
2022-06-08 16:40:08 +0200

    Cookie Delete test

--- src/assets/javascripts/libs/cookie.js
+++ src/assets/javascripts/libs/cookie.js
@@ -51,6 +51,7 @@ class Cookie {
         cookies.forEach(cookie => {
             let eqPos = cookie.indexOf("=");
             let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            console.log(name, cookie);
             if(name != 'PHPSESSID' && name != 'cookiesettings')
                 document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
         });
