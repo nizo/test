fbf6610ac14ce70f762aa1d9cd4b1d62ad0af158
Max Boll <max.boll@callone.de>
2022-06-27 12:37:37 +0200

    Redesign flaconi page

--- src/views/de/case-study-flaconi.php
+++ src/views/de/case-study-flaconi.php
@@ -1,75 +1,123 @@
-<div class="section">
+<div class="section section--colored section--mobile-divider" style="--bg:rgba(255, 181, 186, 0.1);">
     <header class="hero">
         <div class="hero__text">
-            <?= pictureTag(
-                '/assets/images/client-logos/logo-flaconi-2.svg',
-                'Dufte Anrufsteuerung - Flaconi.de',
-                240, 71, [], false
-            ); ?>
-            <h1>Dufte Anrufsteuerung, frei skalierbar</h1>
+            <h1>Dufte Anrufsteuerung, <br class="mobile-hidden" />frei skalierbar</h1>
             <p class="bigtext">Flaconi.de ist einer der größten Onlineversandhändler Deutschlands für Parfum, Pflege und Make-up. Als Premium Online-Shop wird höchster Wert auf ein exzellentes Einkaufserlebnis und erstklassigen Service gelegt.</p>
         </div>
-        <div class="hero__image">
-            <div id="lottie"></div>
-            <?php $additional_js_files[] = '/assets/animations/svg/business.js' ?>
+        <div class="hero__image centered">
+            <div style="background-image:url(/assets/images/photos/lbs-image.jpg);background-size:cover;background-position:center;padding:var(--gutter-xl) var(--gutter-m);border-radius:10px">
+                <div class="blurry-logo-box">
+                    <?= pictureTag(
+                        '/assets/images/client-logos/carddeck/flaconi-carddeck-black.png',
+                        'Check24 ist zufriedener Kunde von CallOne',
+                        150, 80, [
+                            'style' => '--w:150px'
+                        ], false
+                    ); ?>
+                </div>
+            </div>
         </div>
     </header>
+
+    <div class="anchors">
+		<div class="anchors__title">
+			<strong>Schnelleinstieg:</strong> Wofür interessieren Sie sich?
+		</div>
+		<div class="anchors__links">
+			<a href="#anforderungen">Anforderungen</a>
+			<a href="#loesung">Lösung</a>
+			<a href="#ergebnis">Ergebnis</a>
+		</div>
+	</div>
 </div>
 
-<div class="section section--light-green">
+<div class="section section--colored" style="--bg:rgba(255, 181, 186, 0.1)">
+    <div class="section__content section__content--narrow">
+        <h2 class="centered mobile-left">Flaconi.de ist einer der größten Onlineversandhändler Deutschlands...</h2>
+        <p class="centered mobile-left bigtext">...für Parfum, Pflege und Make-up. Als Premium Online-Shop wird höchster Wert auf ein exzellentes Einkaufserlebnis und erstklassigen Service gelegt.</p>
+    </div>
+</div>
+
+<div class="section" style="background-image:linear-gradient(rgba(255, 181, 186, 0.1) 50%, rgba(255, 181, 186, 0) 50%);">
     <div class="section__content section__content--wide">
-        <div class="content-box content-box--white content-box--shadow content-box--full-width">
-            <div class="quote" style="--fontSize:36px">
-                „Wir haben klare Ansprüche an unsere Telefonielösung: erstens soll unsere Vorstellung von optimalem Service auch an telefonischer Schnittstelle umgesetzt werden und zweitens benötigten wir Steuerungs- und Statistikfunktionen für interne Überprüfungen.“
+        <div class="breather-box" style="--c-bg:#FFB5BA;--c-text:#000">
+            <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--3-sm co-grid__col--vcentered">
+                    <?= pictureTag(
+                        '/assets/images/client-logos/logo-flaconi.svg',
+                        'Kundenstimme von Mark Bensch zur CallOne Callcenter Software',
+                        255, 75, [
+                            'style' => 'width:255px !important'
+                        ]
+                    ); ?>
+                </div>
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--9-sm co-grid__col--vcentered">
+                    <h4>flaconi über die CallOne Lösung:</h4>
+                    <div class="quote" style="--fontSize:18px">
+                        „Wir haben klare Ansprüche an unsere Telefonielösung: erstens soll unsere Vorstellung von optimalem Service auch an telefonischer Schnittstelle umgesetzt werden und zweitens benötigten wir Steuerungs- und Statistikfunktionen für interne Überprüfungen.“
+                    </div>
+                    <p>
+                        <strong>Mark Bensch</strong><br />
+                        Chief Technology Officer
+                    </p>
+                </div>
             </div>
-            <p>
-                <strong>Mark Bensch</strong><br />
-                Chief Technology Officer
-            </p>
         </div>
     </div>
 </div>
 
 <div class="section">
-    <div class="section__content section__content--narrow">
-        <p class="centered mobile-left bigtext">Flaconi.de ist einer der größten Onlineversandhändler Deutschlands für Parfum, Pflege und Make-up. Als Premium Online-Shop wird höchster Wert auf ein exzellentes Einkaufserlebnis und erstklassigen Service gelegt.</p>
+    <div class="section__content section__content--narrow centered mobile-left" id="anforderungen">
+        <h2>Anforderungen</h2>
+        <p class="bigtext">Die alte Telefonanlage sollte gegen eine neue Lösung ausgetauscht werden, die in Bezug auf Funktionalität, Skalierung und Statistikauswertung mehr Möglichkeiten bietet. Außerdem war schnell klar, dass hardwaregebundene Lösungen, die das obige Setting bereithalten, das vorgesehene Budget im Bereich der Anfangsinvestition, des Betriebs und der Wartung bei weitem überstiegen. Somit war das Mittel der Wahl eine netzbasierte (Cloud-)Lösung.</p>
     </div>
 </div>
 
-<div class="section">
-    <div class="section__content section__content--narrow centered mobile-left">
-        <h2>Anforderungen</h2>
-        <p class="bigtext">Die alte Telefonanlage sollte gegen eine neue Lösung ausgetauscht werden, die in Bezug auf Funktionalität, Skalierung und Statistikauswertung mehr Möglichkeiten bietet. Außerdem war schnell klar, dass hardwaregebundene Lösungen, die das obige Setting bereithalten, das vorgesehene Budget im Bereich der Anfangsinvestition, des Betriebs und der Wartung bei weitem überstiegen. Somit war das Mittel der Wahl eine netzbasierte (Cloud-)Lösung.</p>
-        <br />
-        <br />
+<div class="section section--colored" style="--bg:rgba(255, 181, 186, 0.1)">
+    <div class="section__content section__content--narrow centered mobile-left" id="loesung">
         <h2>Lösung</h2>
         <p class="bigtext">Durch die funktionale Verknüpfung von Telefonanlage, ACD, Routingfunktionen und Statistiken in einem System, stellten die Lösungen von CallOne einen idealen Lösungsansatz für Flaconi dar.</p>
         <p class="bigtext">Durch die Nutzung der CallOne-Lösung konnte auf der einen Seite die hohen Ansprüche in Bezug auf erstklassigen Service auch am Telefon, als auch die umfangreichen Auswertungsanforderungen im Bereich der Statistiken erfüllt werden. Die Echtzeitauswertung der eingehenden Statistikdaten bieten Flaconi die Möglichkeit, in sekundenschnelle auf abweichende Servicelevel zu reagieren. So wurden umfangreiche und ganz individuelle Statistikreports entwickelt, die automatisiert den entsprechenden Verantwortlichen zugehen.</p>
-        <br />
-        <br />
-        <br />
-        <br />
+    </div>
+</div>
+
+<div class="section">
+    <div class="section__content section__content--narrow centered mobile-left" id="ergebnis">
         <h2>Ergebnis</h2>
         <p class="bigtext">Erstmalig ist Flaconi in der Lage, umfangreiche Statistiken im Bereich des telefonischen Kundenkontaktes auszuwerten. Dies führte sehr schnell zu weitreichenden Erkenntnissen, wie der Kundendialog in Zukunft noch besser gesteuert werden könnte. In enger Zusammenarbeit mit CallOne wurden Daten ausgewertet und neue Abfragemöglichkeiten entwickelt. Die Auswertung führte dazu, dass neue Anrufsteuerungsmechanismen implementiert wurden, die nun dafür sorgen, dass sehr viel bessere Servicelevel erreicht werden.</p>
         <p class="bigtext">Durch den Einsatz der Lösungen von CallOne konnte innerhalb von sechs Wochen die Erreichbarkeit um 15% verbessert werden. Die Abruchquote von Anrufen konnte um 23% gesenkt werden.</p>
     </div>
 </div>
 
-<div class="section section--light-green">
+<div class="section" style="background-image:linear-gradient(rgba(255, 181, 186, 0) 50%, rgba(255, 181, 186, 0.1) 50%);">
     <div class="section__content section__content--wide">
-        <div class="content-box content-box--white content-box--shadow content-box--full-width">
-            <div class="quote" style="--fontSize:36px">
-                „Als Onlineunternehmen messen wir uns an Zahlen und Statistiken. Mit dem Telefoniesystem der CallOne haben wir jetzt eine Lösung gefunden, die alle unsere Ansprüche erfüllt.“
+        <div class="breather-box" style="--c-bg:#FFB5BA;--c-text:#000">
+            <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--3-sm co-grid__col--vcentered">
+                    <?= pictureTag(
+                        '/assets/images/client-logos/logo-flaconi.svg',
+                        'Kundenstimme von Mark Bensch zur CallOne Callcenter Software',
+                        255, 75, [
+                            'style' => 'width:255px !important'
+                        ]
+                    ); ?>
+                </div>
+                <div class="co-grid__col co-grid__col--12-xs co-grid__col--9-sm co-grid__col--vcentered">
+                    <h4>flaconi über die CallOne Lösung:</h4>
+                    <div class="quote" style="--fontSize:18px">
+                        „Als Onlineunternehmen messen wir uns an Zahlen und Statistiken. Mit dem Telefoniesystem der CallOne haben wir jetzt eine Lösung gefunden, die alle unsere Ansprüche erfüllt.“
+                    </div>
+                    <p>
+                        <strong>Mark Bensch</strong><br />
+                        Chief Technology Officer
+                    </p>
+                </div>
             </div>
-            <p>
-                <strong>Mark Bensch</strong><br />
-                Chief Technology Officer
-            </p>
         </div>
     </div>
 </div>
 
-<div class="section">
+<div class="section section--colored-gradient" style="--bg1:rgba(255, 181, 186, 0.1);--bg2:rgba(255, 181, 186, 0)">
     <div class="section__content section__content--narrow">
         <h2 class="centered mobile-left">Über Flaconi</h2>
         <p class="centered mobile-left bigtext"><a href="https://www.flaconi.de" class="inline-link" target="_blank">Flaconi</a> bietet seit Juli 2011 in seinem Online-Shop Flaconi.de eine umfangreiche Auswahl an Marken-Parfums, Pflegeprodukten und Make-up. Neben einem perfekten Kundenservice, einem hochwertigen Design und einer intuitiven Bedienung machen auch vielseitige Produktinformationen den Einkauf im Internet zu einem emotionalen, einzigartigen Erlebnis.</p>
