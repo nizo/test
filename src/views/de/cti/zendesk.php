51ac8d0ca8d033e89b36505373183f69ca7863fc
Max Boll <max.boll@callone.de>
2022-06-29 16:31:56 +0200

    Added some backgrounds

--- src/views/de/cti/zendesk.php
+++ src/views/de/cti/zendesk.php
@@ -14,7 +14,7 @@
     </header>
 </div>
 
-<div class="section">
+<div class="section section--light-green">
     <div class="section__content section__content--narrow centered mobile-left">
         <h2>Voice Tickets</h2>
         <p class="bigtext">Integrieren Sie Voice Tickets in Zendesk für effizienteren Kundenservice.</p>
@@ -23,32 +23,38 @@
     <div class="section__content section__content--wide centered mobile-left">
         <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
             <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
-                <span class="sl sl-chat-acknowledge sl-size-xl"></span>
-                <h3>Voice Tickets für eingehende Anrufe</h3>
-                <p>Mit der Zendesk-Integration erhalten Sie für jeden eingehenden Anruf automatisch ein Voice Ticket in Zendesk.</p>
+                <div class="content-box content-box--white content-box--shadow">
+                    <span class="sl sl-chat-acknowledge sl-size-xl"></span>
+                    <h3>Voice Tickets für eingehende Anrufe</h3>
+                    <p>Mit der Zendesk-Integration erhalten Sie für jeden eingehenden Anruf automatisch ein Voice Ticket in Zendesk.</p>
+                </div>
             </div>
             <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
-                <span class="sl sl-king sl-size-xl"></span>
-                <h3>Tickets direkt dem Kunden zuordnen</h3>
-                <p>Voice Tickets werden anhand der Rufnummer automatisch bestehenden Kunden zugeordnet, können manuell von Ihnen zugeordnet werden oder werden als neues Ticket erstellt.</p>
+                <div class="content-box content-box--white content-box--shadow">
+                    <span class="sl sl-king sl-size-xl"></span>
+                    <h3>Tickets direkt dem Kunden zuordnen</h3>
+                    <p>Voice Tickets werden anhand der Rufnummer automatisch bestehenden Kunden zugeordnet, können manuell von Ihnen zugeordnet werden oder werden als neues Ticket erstellt.</p>
+                </div>
             </div>
             <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md">
-                <span class="sl sl-ripped-note sl-size-xl"></span>
-                <h3>Outbound Anrufe als Tickets</h3>
-                <p>Auch Ihre ausgehenden Anrufe können Sie als Ticket in Zendeskintegrieren und bekommen dazu alle Informationen zum Anruf mitgegeben.</p>
+                <div class="content-box content-box--white content-box--shadow">
+                    <span class="sl sl-ripped-note sl-size-xl"></span>
+                    <h3>Outbound Anrufe als Tickets</h3>
+                    <p>Auch Ihre ausgehenden Anrufe können Sie als Ticket in Zendeskintegrieren und bekommen dazu alle Informationen zum Anruf mitgegeben.</p>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<div class="section" id="anbindungen">
+<div class="section section--light-green-white" id="anbindungen">
     <div class="section__content section__content--wide">
         <h2 class="centered mobile-left">
             ACD:
             Optimierte Kundenprozesse mit der CallOne Cloud ACD und Zendesk
         </h2>
 
-        <div class="content-box content-box--full-width content-box--shadow">
+        <div class="content-box content-box--white content-box--full-width content-box--shadow">
             <div class="co-grid co-grid--no-margin-top co-grid--no-margin-bottom">
                 <div class="co-grid__col co-grid__col--12-xs co-grid__col--4-md co-grid__col--vcentered">
                     <?= pictureTag(
@@ -62,8 +68,10 @@
                 <div class="co-grid__col co-grid__col--12-xs co-grid__col--8-md co-grid__col--vcentered">
                     <h3>Zuordnung von Tickets zu Agenten</h3>
                     <p>Durch Hinterlegen der Zendesk User-ID in Ihrer CallOne Cloud ACD, können Anrufe der Agenten völlig automatisiert dem richtigen Zendesk-User zugeordnet werden.</p>
+                    <br />
                     <h3>Implementierung über ID und Token</h3>
                     <p>Um Ihnen den ZendeskKonnektor freizuschalten, benötigen wir lediglich Ihre Zendesk-ID, einen Admin-User sowie ein generiertes Token.</p>
+                    <br />
                     <h3>Überblick über Callcenter-Daten</h3>
                     <p>Mit Hilfe des ZendeskKonnektor finden Sie in Zendeskalle Informationen zu angewählten Agenten, Skillgruppen, Hotlines und Warteschleife.</p>
                 </div>
@@ -84,8 +92,10 @@
                 <div class="co-grid__col co-grid__col--12-xs co-grid__col--8-md co-grid__col--order-2-xs co-grid__col--vcentered">
                     <h3>Gruppenfunktionen</h3>
                     <p>Unterteilen Sie z.B. Ihre Hotline-Rufnummern in verschiedene Gruppen, um Anrufe bestmöglich in Zendeskauswerten zu können.</p>
+                    <br />
                     <h3>Übergabe individueller Parameter</h3>
                     <p>Auf Wunsch übergeben wir Ihnen über die Schnittstelle auch individuelle Parameter wie Eingaben in IVR-Menüs, die Sie z.B. in Zendesk-Feldern oder Tickets anzeigen lassen können.</p>
+                    <br />
                     <h3>Integration von Statistiken</h3>
                     <p>Integrieren Sie alle ACD- und Rufnummernstatistiken ganz einfach in Zendesk, um den Überblick über alle telefonischen Kontakte mit Ihren Kunden zu haben.</p>
                 </div>
