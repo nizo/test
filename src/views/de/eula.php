e0296c1e5ca8afa39786f50670471d1d005f13ce
Max Boll <max.boll@callone.de>
2022-10-12 11:11:16 +0200

    Added Softphone Eula

---
+++ src/views/de/eula.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="de">
+<head>   
+    <meta charset="utf-8"/>
+    <title>EULA - Cloud Callcenter Software, VoIP-Telefonanlagen, weltweite Rufnummern | CallOne</title>
+    <meta name="description" content="VoIP-Telefonanlage und Callcenter Software &quot;all-in-one&quot; mit CTI-Integration aus der deutschen Cloud. Keine Hardware oder Softphones notwendig. Jetzt testen!" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta name="google-site-verification" content="Sqndy6_j8hwq2O67oeDnHTdNmKPDaCRcC-g_K3FRYfE" />
+
+    <meta property="og:title" content="EULA - Cloud Callcenter Software, VoIP-Telefonanlagen, weltweite Rufnummern"/>
+    <meta property="og:site_name" content="Callone"/>
+    <meta property="og:locale" content="de_DE"/>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.callone.de/rechtliches/softphone_eula.html" />
+
+    <meta property="og:image" content="https://www.callone.de/images/og/1200x630/default/Cloud_Callcenter_Software-_VoIP-__Telefonanlagen.png" />
+    <meta property="twitter:image" content="https://www.callone.de/images/og/1200x600/default/Cloud_Callcenter_Software-_VoIP-__Telefonanlagen.png">	
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="https://www.callone.de/assets/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://www.callone.de/assets/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://www.callone.de/assets/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="https://www.callone.de/assets/images/favicons/site.webmanifest">
+    <link rel="mask-icon" href="https://www.callone.de/assets/images/favicons/safari-pinned-tab.svg" color="#86ed18">
+    <link rel="shortcut icon" href="https://www.callone.de/assets/images/favicons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="CallOne">
+    <meta name="application-name" content="CallOne">
+    <meta name="msapplication-TileColor" content="#86ed18">
+    <meta name="msapplication-TileImage" content="https://www.callone.de/assets/images/favicons/mstile-144x144.png">
+    <meta name="msapplication-config" content="https://www.callone.de/assets/images/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <meta name="author" content="Callone GmbH" />
+    <meta name="revisit-after" content="3 days" />
+    <meta name="robots" content="index,follow" />
+
+    <link rel="preconnect" href="https://googleads.g.doubleclick.net">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://www.google-analytics.com">
+    <link rel="preconnect" href="https://www.googletagmanager.com">
+    <link rel="preconnect" href="https://www.google.de">
+    <link rel="preconnect" href="https://www.google.com">
+    <link rel="preconnect" href="https://www.googleadservices.com">
+
+    <link rel="stylesheet" href="/assets/stylesheets/application.min.css?build=347638e6959bb7efb074f110d901ef32" />
+
+    
+    <script type="application/ld+json">
+    {
+        "@context" : "http://schema.org",
+        "@type" : "Organization",
+        "name" : "CallOne",
+        "url" : "https://www.callone.de",
+        "sameAs" : [
+            "https://de.linkedin.com/company/callone-gmbh",
+            "https://de-de.facebook.com/CallOneGmbH/"
+        ]
+    }
+    </script>
+</head>
+<body>
+
+<div class="section">
+    <div class="section__content section__content--narrow">
+        <h1 class="centered mobile-left">Endbenutzer-Lizenzvertrag (EULA)</h1>
+        <br />
+        <p class="subtext">
+            für das CallOne Softphone (bezeichnet als „Software“)<br />
+            WICHTIG! – BITTE LESEN SIE DIE BEDINGUNGEN DIESER LIZENZVEREINBARUNG SORGEFÄLTIG, bevor sie die Installation des Programms fortsetzen. 
+        </p>
+        
+        <p>
+            Dieser Endbenutzerlizenzvertrag (dieser „Vertrag“) ist ein Rechtsvertrag zwischen Ihnen, im eigenen Namen oder im Namen einer beliebigen Organisation handelnd, für die Sie das oben genannte Produkt erhalten haben (kollektiv „Lizenznehmer“), und 
+        </p>
+
+        <p>
+            CallOne GmbH
+            Posthofstr. 5
+            14467 Potsdam
+            Telefon: +49 30 920 33 500
+            E-Mail: kontakt@callone.de
+            Amtsgericht Berlin Charlottenburg.  HRB 226944 B
+            („Lizenzgeber“).
+        </p>
+
+        <p>CallOne besitz alle Markenschutzrechte und Eigentumsrechte, einschließlich Copyrights in der Software und für die Software und aller Kopien davon (einschließlich und ohne Beschränkung aller in die Software integrierten Bildern sowie Video und Audiotext) und begleitender Datenträger und gedruckter Materialien. Das Kopieren der Software oder eines Teils davon ist durch diese Gesetze und Abkommensbestimmungen streng verboten. </p>
+        <p>Der Endbenutzer-Lizenzvertrag gilt für die Installation der CallOne Softphone Software auf Ihrem PC, Mac oder mobilen Endgerät. Mit der Installation und Nutzung der Software stimmen Sie den in diesem Vertrag aufgeführten Bedingungen zu. Die Bedingungen müssen durch Sie zugestimmt werden, bevor Sie das Produkt nutzen können. Dieser Endbenutzer-Lizenzvertrag legt ausschließlich Ihre Rechte und Pflichten als Endbenutzer dieser Software dar. Der Vertrag hat keinerlei Auswirkungen auf die Nutzung der CallOne Telefonanlage und dem zwischen dem Lizenzgeber und dem Kunden des Lizenzgeber („Kunde“) geschlossenen Vertrag.</p>
+        <p>Durch Bestätigung der Schaltfläche „Zustimmen“ erkennen Sie den Vertrag für als bindend an. Sollten Sie mit den Bedingungen nicht einverstanden sein, brechen Sie bitte die Installation des Softphones ab.</p>
+        <p>
+            HINWEIS: <br />
+            Die Software bietet keinen Ersatz für ein Mobil- oder Festnetztelefon. Dies bedeutet unter anderem auch, dass ein Notruf nur dann abgesetzt werden kann, wenn die Software über eine stabile und funktionierende Breitbandverbindung verfügt. Treffen Sie bitte daher entsprechende Vorkehrungen, um im Bedarfsfall einen Notruf absetzen zu können.
+        </p>
+
+        <hr class="divider">
+
+        <h1 class="centered mobile-left">Lizenzbestimmungen</h1>
+
+        <h2>1. Nutzung </h2>
+        <p>1.1. Bei Annahme der Bestimmungen dieses Vertrags gewährt CallOne dem Lizenznehmer hiermit ein begrenztes, persönliches, nicht übertragbares, nicht exklusives Recht zur Nutzung der Software. Alle dem Lizenznehmer nicht ausdrücklich lizenzierten Rechte in diesem Vertrag sind CallOne vorbehalten. </p>
+        <p>1.2. Durch diesen Vertrag darf der Lizenznehmen das lizensierte Produkt immer nur auf einem einzelnen Gerät nutzen. </p>
+        <p>1.3. Bei dem Produkt handelt es sich um ein geschütztes Produkt des Lizenzgebers, das durch verschiedene Gesetze bezüglich des geistigen Eigentums, einschließlich des Urheberrechtes, geschützt ist. Sie dürfen die Software und Dokumentation ausschließlich als Teil des Produktes, in welchem die Software und die Dokumentation enthalten sind oder in der Form, in der sie geliefert wurden, nutzen. Ihre gesetzlichen Rechte, insbesondere Ihr Recht, eine Sicherheitskopie zu erstellen oder die Anwendung unter den Bedingungen der § 69d und 69e des Urheberrechtsgesetzes zu dekompilieren und Fehler in der Anwendung zu beheben, bleiben davon unberührt. Ohne die vorhergehende schriftliche Zustimmung des Lizenzgebers werden Sie keiner dritten Partei Ergebnisse eines Benchmarktests des Produktes offenbaren.</p>
+        <p>1.4. Der Lizenznehmer erkennt an, dass die Software das ausschließliche Eigentum von CallOne und deren Lizenzgeber ist und wertvolle Geschäftsgeheimnisse enthält. Der Lizenznehmer erklärt sich bereit, die Software in Übereinstimmung mit den Bestimmungen dieses Vertrags streng vertraulich zu unterhalten. Der Lizenznehmer stimmt zu, dass jegliches Versäumnis bezüglich des streng vertraulichen Unterhalts der Software seitens des Lizenznehmers CallOne irreparable Schäden zufügt, und CallOne daher einen Anspruch auf angemessene Entschädigung, ohne irreparable Schäden zu demonstrieren oder eine Kaution zu erbringen.</p>
+        <p>1.5. Der Lizenznehmer nimmt zur Kenntnis, dass CallOne nicht personenbezogene identifizierbare Informationen über Ihre Produkte und die Verwendung der Produkte erheben und aufzeichnen kann, um CallOne bei der Entwicklung neuer Produkte und der Verbesserung des Betriebs und der Funktion Ihrer Produkte zu unterstützen. Der Lizenzgeber ist berechtigt, diese Informationen in anonymisierter Form zur Verbesserung seiner Produkte oder im Rahmen der Bereitstellung von Services bzw. Technologien an Sie zu nutzen sowie an Dritte weiterzugeben.</p>
+        <p></p>
+        <p><h2>2. Einschränkungen</h2></p>
+        <p>Der Lizenznehmer darf die Software oder Teile davon nicht vermieten, verleasen oder verleihen. Das Disassemblieren und/oder Patchen der Software oder seiner Hilfedateien sowie das Verändern oder Austauschen von Modulen ist generell untersagt. Der Lizenznehmer darf die Rechte unter diesem Vertrag oder Teile davon nicht übertragen oder einer Drittperson, die nicht Partei dieses Vertrags ist, Zugang dazu erteilen. Der Lizenznehmer darf die Software oder seiner Hilfedateien nicht zurückentwickeln, dekompilieren, disassemblieren, modifizieren, übersetzen, versuchen den Quellencode zu ermitteln oder von dieser Software abgeleitete Werke erzeugen, ausgenommen und nur soweit, wie solche Aktivitäten durch anwendbares Recht, obgleich dieser Einschränkung, ausdrücklich zugelassen sind. Der Lizenznehmer darf die Software nicht in eine andere Software oder ein mit Software verwandtes Produkt integrieren. </p>
+        <p></p>
+        <p><h2>3. Produktwartung</h2></p>
+        <p>Gemäß diesem Vertrag ist der Lizenzgeber nicht verpflichtet, Ihnen die Wartung, Support oder Updates für die Software zur Verfügung zu stellen. Gesonderte Vereinbarung mit dem Kunden bleiben hiervon ausgeschlossen.</p>
+        <p></p>
+        <p><h2>4. Gewährleistungsausschluss </h2></p>
+        <p>4.1. Der Lizenzgeber gewährleistet oder garantiert nicht, dass die Software für die Zwecke des Endnutzers in jedem Fall geeignet ist, insbesondere, dass die Software mit allen Fremdkomponenten, wie Datenbanken und Hardware-Treibern, fehlerfrei funktioniert.</p>
+        <p>4.2. Der Lizenzgeber ist nicht verpflichtet die Software in einer bestimmten Art oder Güte zur Verfügung zu stellen. Durch Akzeptieren der Endbenutzer-Lizenzvereinbarung akzeptieren Sie den IST-Zustand der Software, ohne da daraus eine bestimmte Beschaffenheit der Software vereinbart oder geschuldet wird.</p>
+        <p>4.3. Der Lizenzgeber ist berechtigt, vorbehaltlich anderer Absprachen mit dem Kunden, die Lizenz an dem Produkt, Ihnen gegenüber jederzeit zurückziehen. </p>
+        <p>4.4. Die Gewährleistung ist ausgeschlossen. </p>
+        <p></p>
+        <p><h2>5. Haftungsausschluss</h2></p>
+        <p>Abgesehen von Ansprüchen nach dem Produkthaftungsgesetz, Fällen von Vorsatz oder grober Fahrlässigkeit des Lizenzgebers oder von ihm zurechenbaren dritten, und von der Verletzung wesentlicher vertragspflichten, des Körpers oder Gesundheit ist die Haftung des Lizenzgebers ausgeschlossen. Betreffend dieser Nutzungsbedingungen sind wesentliche vertragspflichten solche, die die Durchführung des Vertrags erst ermöglichen und bei denen sie berechtigt darauf vertrauen können, dass diese durch den Lizenzgeber erfüllt werden.</p>
+        <p></p>
+        <p><h2>6. Haftungsbeschränkung, Schadloshaltung</h2></p>
+        <p>Soweit gesetzlich zulässig, haften weder der Lizenzgeber noch die Vertriebspartner für mittelbare oder unmittelbare Schäden, Mangelfolgeschäden, Schäden durch Unmöglichkeit der Nutzung der Software sowie Schäden aufgrund des Verlusts von Geschäftswert, Arbeitsunterbrechungen, Computerausfällen oder -fehlfunktionen oder für sonstige geschäftliche Schäden oder Verluste jeder Art, auch wenn der Lizenzgeber auf die Möglichkeit derartiger Schäden hingewiesen wurde, oder für sonstige Forderungen dritter, unabhängig davon, ob die Möglichkeit derartiger Schäden zu erwarten war.</p>
+        <p>In jedem Falle ist die Haftung auf den jeweiligen Rechnungswert beschränkt. </p>
+        <p>Die Haftung beschränkt sich auf die Nutzer, die dieser Software nachweislich direkt von dem Lizenzgeber oder einem autorisierten Händler bezogen haben. Bei Weiterverkauf der Software übernimmt der Lizenzgeber keine Haftung für Schäden, die dem Empfänger entstehen.</p>
+        <p>Der Lizenznehmer verpflichtet sich, den Lizenzgeber und dessen Handlungsbevollmächtigte, Direktoren, angestellte und Zulieferer von allen Verlusten, Schäden, Geldstrafen und Ausgaben (einschließlich Anwaltshonorare und juristische Kosten), die sich aufgrund oder im Zusammenhang mit einer Nutzung des Produkts ergeben, mit der die Rechte dritter verletzt wurden oder mit der gegen ein Gesetz verstoßen wurde, zu schützen, freizustellen und schadlos zu halten.</p>
+        
+        <h2>7. Spezifischer Haftungsausschluss für Notdienste </h2>
+        <p>Weder der Lizenzgeber noch dessen Handlungsbevollmächtigte, angestellte oder angeschlossene Unternehmen können für eine Forderung, einen Schaden oder einen Verlust haftbar gemacht werden, die sich aus diesem Vertrag, einer Garantie, einer unerlaubten Handlung (einschließlich Fahrlässigkeit) oder einer anderen Art der Haftung durch die folgenden Punkte oder im Zusammenhang mit ihnen ergibt:</p>
+        <p>(i) Unvermögen des Lizenznehmers, das Produkt zur Kontaktierung von Notdiensten zu nutzen oder</p>
+        <p>(ii) Unterlassung des Lizenznehmers, zusätzliche Vorkehrungen zum Zugriff auf Notdienste zu treffen.</p>
+        
+        <h2>8. Gesamter Vertrag</h2>
+        <p>Diese EULA stellt den vollständigen Vertrag zwischen dem Lizenzgeber und dem Empfänger im Hinblick auf den Vertragsgegenstand dar. Sie ersetzt alle vorherigen mündlichen oder schriftlichen Absprachen, Verständigungen oder Vereinbarungen, soweit diese nicht nach diesem Vertrag unberührt bleiben sollen. Diese EULA darf nicht oder nur in schriftlicher Form abgeändert werden, wobei der geänderte Vertrag von einem autorisierten Vertreter des Lizenzgebers ordnungsgemäß unterzeichnet werden muss. Sollten einzelne Bestimmungen dieser EULA unwirksam oder nicht durchsetzbar sein, so lässt dies die Gültigkeit der restlichen Bestimmungen der EULA nach dem übereinstimmenden Parteiwillen unberührt. Anstelle der unwirksamen Bestimmung tritt im Wege ergänzender Vertragsauslegung ein solche, die dem wirtschaftlichen Zweck der unwirksamen Bestimmungen am nächsten kommt.</p>
+        
+        <h2>9. Kündigung</h2>
+        <p>Diese EULA bleibt in Kraft, solange sie nicht gekündigt wird. Sie können diese EULA kündigen, indem Sie alle Kopien des Produktes und die dazugehörige Dokumentation von Ihrem Gerät entfernen und zerstören. Der Lizenzgeber kann diese EULA durch einfache Erklärung kündigen. Das unautorisierte Kopieren des Produktes oder der dazugehörigen Dokumentation oder eine anderweitige Nichtbeachtung der Bedingungen dieses EULA führt deren automatischer Beendigung. Mit Beendigung dieser EULA endet das hierin erteilte Nutzungsrecht an der Softwareanwendung. Sie sind verpflichtet, unverzüglich nach Beendigung der EULA jegliche Nutzung des Produktes und der dazugehörigen Dokumentation und aller Sicherheitskopien davon unverzüglich einzustellen.</p>
+        
+        <h2>10. Sonstiges</h2>
+        <p>Dieser EULA unterliegt dem materiellen Recht von Deutschland. Für Streitigkeiten, die sich aus diesem EULA ergeben, ist Düsseldorf der einzige Gerichtsstand. Das UN-Kaufrecht ist ausgeschlossen. Die Ziffern 1.3, 1.4, 1.5, 3, 4, 5, 6, 7, 8, und 9 bleiben auch nach Beendigung des EULA gültig.</p>
+
+        <p>Die Datenschutzbestimmungen des Lizenzgebers können unter <a href="https://www.callone.de/datenschutz" class="inline-link">https://www.callone.de/datenschutz</a> eingesehen werden.</p>       
+    </div>
+</div>
+
+<script src="/assets/javascripts/application.min.js?build=1b44e39f1d2a8c0c3de1f7e3b1b518ba"></script>
+    
+            <script>
+            document.addEventListener('DOMContentLoaded', () => {
+                let urlQuery = window.location.search;
+                let params = new URLSearchParams(urlQuery);
+                if (!params.has('no-banner') && !cookiesettings.exists('cookiesettings'))
+                    new Modal(null, 'cookie-settings', null, 1000);
+            });
+        </script>
+    </body>
+</html>
