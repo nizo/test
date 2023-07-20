nizo/test

commit 0a7284bb5f0f9ad10bfe54a78306c7181d5eb57b

Update jobs.inc.php

src/libs/jobs.inc.php
@@ -1191,7 +1191,7 @@ $job = new job ('Management', '(Junior) Finance Specialist (w/m/d) I 20 bis 40 S
 	$job->statement_set (new job_statement ('/assets/images/photos/jenny_radziejewski.jpg', 'Jenny Radziejewski', 'Customer Success Management', 'Die große Flexibilität, der enge Kontakt zum Kunden und vor allem der menschliche Umgang untereinander zeichnen meinen Arbeitsalltag bei CallOne aus.'));
 	$jobs->job_add ($job);*/
 
-$job = new job ('Vertrieb', 'Head of HR I HR Generalist (w/m/d) I  4-Tage-Woche und Home Office-Tage möglich', 'Du bist unser*e Ansprechpartner*in in Sachen Recruiting & Employer Branding.');
+/*$job = new job ('Vertrieb', 'Head of HR I HR Generalist (w/m/d) I  4-Tage-Woche und Home Office-Tage möglich', 'Du bist unser*e Ansprechpartner*in in Sachen Recruiting & Employer Branding.');
 	$job->location_set ('Hamburg / Remote');
 	$job->url_set ('job-head-of-hr-in-hamburg');
 	$job->quickinfo_set ($job->location_get().' · unbefristet · (m/w/d)');
@@ -1223,7 +1223,7 @@ $job = new job ('Vertrieb', 'Head of HR I HR Generalist (w/m/d) I  4-Tage-Woche 
 	$job->statement_set (new job_statement ('/assets/images/photos/stefanie_bendig.jpg', 'Stefanie Bendig', 'HR Department', 'Das Schöne an CallOne ist der starke Zusammenhalt. Strategien und Ziele werden gemeinsam in regelmäßigen Meetings festgelegt.'));
 	$job->statement_set (new job_statement ('/assets/images/photos/jenny_radziejewski.jpg', 'Jenny Radziejewski', 'Customer Success Management', 'Die große Flexibilität, der enge Kontakt zum Kunden und vor allem der menschliche Umgang untereinander zeichnen meinen Arbeitsalltag bei CallOne aus.'));
 	$jobs->job_add ($job);
-	return $jobs;
+	return $jobs;*/
 }
 
 

commit e525e09496ffe22adb70ab089bbd8482f2395b03

fix job error introduced in 0a7284b

src/libs/jobs.inc.php
@@ -1223,7 +1223,8 @@ $job = new job ('Management', '(Junior) Finance Specialist (w/m/d) I 20 bis 40 S
 	$job->statement_set (new job_statement ('/assets/images/photos/stefanie_bendig.jpg', 'Stefanie Bendig', 'HR Department', 'Das Schöne an CallOne ist der starke Zusammenhalt. Strategien und Ziele werden gemeinsam in regelmäßigen Meetings festgelegt.'));
 	$job->statement_set (new job_statement ('/assets/images/photos/jenny_radziejewski.jpg', 'Jenny Radziejewski', 'Customer Success Management', 'Die große Flexibilität, der enge Kontakt zum Kunden und vor allem der menschliche Umgang untereinander zeichnen meinen Arbeitsalltag bei CallOne aus.'));
 	$jobs->job_add ($job);
-	return $jobs;*/
+	*/
+	return $jobs;
 }
 
 