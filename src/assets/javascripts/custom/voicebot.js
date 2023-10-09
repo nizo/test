ac05df2f254749523be2f4363301c315496c5ce5
Julian Wright <office@julianwright.de>
2023-10-09 22:01:31 +0200

    add cross domain voicebot conversion event trigger

--- src/assets/javascripts/custom/voicebot.js
+++ src/assets/javascripts/custom/voicebot.js
@@ -302,6 +302,12 @@ function handleSubmit(e) {
         contentForm.style.display = 'none';
         contentSelection.style.display = 'none';
         window.scrollTo(0, contentSuccess.offsetTop - 100);
+		
+		// when called from inopla via iframe, send conversion event to parent iframe
+		if(window.location.href.match(/inopla_style/)) {
+			window.parent.postMessage({ eventName: 'voicebot_anfrage' }, "*");
+		}
+
     })
     .catch(response => {
         console.error(response);
